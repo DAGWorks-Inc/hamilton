0fe773908ea9304808d03ece46bb13b0b81ece86
Stefan Krawczyk <skrawczyk@stitchfix.com>
2019-08-02 17:21:06 -0700

    Modifies creating nodes to pull doc strings into Node objects
    This follows the previous change, and pulls doc strings if
    they exist from the functions into the Node object. Unit
    tests are updated to check for this.

--- dummy_functions.py
+++ dummy_functions.py
@@ -6,21 +6,20 @@ from tests.resources import only_import_me
 
 
 def A(b: int, c: int) -> int:
-    """Function that should be come part of the graph"""
+    """Function that should become part of the graph - A"""
     return b + c
 
 
 def _do_not_import_me(some_input: int, some_input2: int) -> int:
-    """Function that should not become part of the graph."""
+    """Function that should not become part of the graph - _do_not_import_me."""
     only_import_me.this_is_not_something_we_should_import()
     return some_input + some_input2
 
 
 def B(A: int) -> int:
-    """Function that should be come part of the graph"""
+    """Function that should become part of the graph - B"""
     return A * A
 
 
-def C(A: int) -> int:
-    """Function that should be come part of the graph"""
+def C(A: int) -> int:  # empty string doc on purpose.
     return A * 2
