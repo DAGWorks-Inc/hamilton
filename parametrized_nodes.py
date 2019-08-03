e2dc041f03d295a4cbf22fffd7b53b2d1f1e8a6d
Stefan Krawczyk <skrawczyk@stitchfix.com>
2019-08-02 17:25:55 -0700

    Flips parameterized decorator and adds requirement to handle doc strings
    1. We flip the order of what’s passed in to the parameterized decorator. We did
    this because after discussing, we think it’s more natural for people to think of the
    column name first, and then to specify the value, then go the other way around.
    E.g. more people think about the event name, then necessarily the date that it happened.
    2. We add the requirement to provide documentation. This is necessary if we want to
    create DRY code and also ensure that people document it. That way when we come
    to display/use it all the functions have doc strings.

--- parametrized_nodes.py
+++ parametrized_nodes.py
@@ -1,7 +1,7 @@
 from hamilton.function_modifiers import parametrized
 
 
-@parametrized('param', {1: 'parametrized_1', 2: 'parametrized_2', 3: 'parametrized_3'})
+@parametrized('param', {('parametrized_1', 'doc'): 1, ('parametrized_2', 'doc'): 2, ('parametrized_3', 'doc'): 3})
 def to_parametrize(param: int) -> int:
     """Function that should be parametrized to form multiple functions"""
     return param
