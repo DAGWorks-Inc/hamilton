35c8dcee78dac21db019f5f2b5356cfe54ca2ceb
Stefan Krawczyk <skrawczyk@stitchfix.com>
2019-09-05 11:32:24 -0700

    Adds ops_forecast column
    This required fixing how we think about segment filters in the other code that I was
    writing. Since we don’t assume that segment filters have region just yet.
    
    Otherwise this seems to match exactly with the columns coming out of the ops_forecast
    related data. Only thing that we don’t support yet for this is segmenting by “size”.
    
    Future thought: we need to better handle segment filters and ensure that downstream functions know what
    to use/pull out.

--- function_modifiers.py
+++ function_modifiers.py
@@ -278,9 +278,8 @@ class model(NodeExpander):
 
     def get_nodes(self, fn: Callable, config: Dict[str, Any] = None) -> Collection[node.Node]:
         if self.config_param not in config:
-            return []
-            # raise InvalidDecoratorException(f'Configuration has no parameter: {self.config_param}. Did you define it? If so did you spell it right?')
-
+            # return []
+            raise InvalidDecoratorException(f'Configuration has no parameter: {self.config_param}. Did you define it? If so did you spell it right?')
         model = self.model_cls(config[self.config_param], **self.extra_model_params)
         return [node.Node(
             name=fn.__name__,
