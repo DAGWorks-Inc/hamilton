1e9ca77fc99f66d077887428b2665184503e501c
Elijah ben Izzy <elijah.benizzy@stitchfix.com>
2019-09-04 17:12:50 -0700

    Implements @config.when decorator for configuration-driven columns
    This allows us to have programmatically-driven column relationships.
    To do this, we create a new abstraction, FunctionResolver, that
    allows us to resolve a function before modifying it. While this design is not
     perfect, this unblocks the use case we need.
    Current limitations:
    - No two functions can have the same name (or they'll be overwritten)
    - We can't have a default function implementation. E.G. we have to cover the full space
    of configuration possibilities through config.when() decorators. To implement this properly
    we'll have to have a better set of abstractions and put more thought in.

---
+++ config_modifier.py
@@ -0,0 +1,20 @@
+from hamilton.function_modifiers import config
+
+
+def new_param() -> str:
+    return 'dummy'
+
+
+@config.when(fn_1_version=0)
+def fn() -> str:
+    pass
+
+
+@config.when(fn_1_version=1)
+def fn__v1() -> str:
+    return 'version_1'
+
+
+@config.when(fn_1_version=2)
+def fn__v2(new_param: str) -> str:
+    return 'version_2'
