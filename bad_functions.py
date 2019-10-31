1656a862d0a71729509237dc802e8a901e479932
Stefan Krawczyk <skrawczyk@stitchfix.com>
2019-10-31 15:50:20 -0700

    Handles 3.7 issubclass breaking change
    So previously in python 3.6:
    
    ```python
    from typing import Dict
    issubclass(dict, Dict)  # True
    issubclass(Dict, dict) # True
    ```
    
    In python 3.7+
    ```python
    from typing import Dict
    issubclass(dict, Dict)  # True
    issubclass(Dict, dict) # False  <—— !
    ```
    
    So in order to preserve 3.6 and 3.7 operations we
    special case looking at dict & list types. I added
    a unit test to catch the dict case at least — this is how we
    found this issue in the first place.
    
    I rearranged the logic a little to make it a little easier to parse
    the boolean logic of when we would throw an error.

--- bad_functions.py
+++ bad_functions.py
@@ -13,3 +13,13 @@ def A(b, c) -> int:
 def B(b: int, c: int):
     """Should error out because we're missing an annotation"""
     return b + c
+
+
+def C(b: int) -> dict:
+    """Setting up type mismatch."""
+    return {'hi': 'world'}
+
+
+def D(C: int) -> int:
+    """C is the incorrect type."""
+    return int(C['hi'])
---
+++ typing_vs_not_typing.py
@@ -0,0 +1,32 @@
+"""
+Module for dummy functions to test graph things with.
+"""
+# we import this to check we don't pull in this function when parsing this module.
+from tests.resources import only_import_me
+from typing import Dict
+
+
+def A(b: int, c: int) -> Dict:
+    """Function that outputs a typing type."""
+    return {'a': b + c}
+
+
+def _do_not_import_me(some_input: int, some_input2: int) -> int:
+    """Function that should not become part of the graph - _do_not_import_me."""
+    only_import_me.this_is_not_something_we_should_import()
+    return some_input + some_input2
+
+
+def B(A: dict) -> int:
+    """Function that depends on A, but says it's a primitive type dict."""
+    return A['a'] + 1
+
+
+def A2(x: int, y: int) -> dict:
+    """Graph function using primitive output type."""
+    return {'a': x + y}
+
+
+def B2(A2: Dict) -> int:
+    """Graph function depending on A2 but saying it's a typing type."""
+    return A['a'] + 1
