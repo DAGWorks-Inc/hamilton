43fcb017e7b4e5a33f35e883feab85da10c080c5
Elijah ben Izzy <elijah.benizzy@stitchfix.com>
2019-11-04 17:11:25 -0800

    Implements dynamic configuration loading
    Previously we were evaluating the entire config before executing the DAG.
    Thus, items we evaluated were not actually being used. With this change we
    evaluate these items dynamically to ensure that we don't have to pay the cost when
    we don't need to.
    
    Furthermore, we've narrowed down the space of configuration possibilities. We no longer
    allow default json configs, to make it simpler. We can always add back in if we'd like.

--- config/default_config.py
+++ config/default_config.py
@@ -1,14 +1,9 @@
-def key_overwrite_not_ignoring_input(key_to_overwrite: str) -> str:
-    return key_to_overwrite
+_VALUE = 'value'
 
 
-def key_overwrite_ignoring_input(key_to_overwrite_2: str) -> str:
-    return key_to_overwrite_2
+def key() -> str:
+    return _VALUE
 
 
-def key_fn_not_to_overwrite() -> str:
-    return 'default_value'
-
-
-def key_not_to_overwrite_with_params(key_not_to_overwrite: str) -> str:
-    return 'default_value'
+def double_key(key: str) -> str:
+    return key + key
