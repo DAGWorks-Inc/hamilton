72d3431f242faa63ec664405d2a87e8b89f211c9
Stefan Krawczyk <stefan@idibon.com>
2022-02-02 21:39:34 -0800

    Refactors DaskExecutor to DaskGraphAdapter
    This moves things to an experimental package -- to make it clear
    that these are early bits of code.
    
    It then implements the HamiltonGraphAdapter interface, adds
    tests, and a hello world.
    
    The salient thing to note about the hello world is that we
    introduce a data_loaders and a business_logic modules. These
    are to separate hamilton logic functions. The idea is that business_logic
    is fairly fixed and invariant, and then if you wanted to go from local development
    to running on a dask cluster, you'd either include/change the data_loaders
    module for some other module that loaded things the way you want them to.

---
+++ examples/dask/hello_world/run.py
@@ -0,0 +1,49 @@
+import importlib
+import logging
+import sys
+
+from hamilton import base
+from hamilton import driver
+from hamilton.experimental import h_dask
+
+
+logger = logging.getLogger(__name__)
+
+
+if __name__ == '__main__':
+    from hamilton import log_setup
+    log_setup.setup_logging()
+    # easily replace how data is loaded by replacing the data_loaders module
+    module_names = ['business_logic', 'data_loaders']
+
+    from dask.distributed import Client, LocalCluster
+    # Setup a local cluster.
+    # By default this sets up 1 worker per core
+    cluster = LocalCluster()
+    client = Client(cluster)
+    logger.info(client.cluster)
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+    modules = [importlib.import_module(m) for m in module_names]
+    dga = h_dask.DaskGraphAdapter(client, base.PandasDataFrameResult(), visualize=True)
+    # will output mydask.png
+
+    initial_config_and_data = {
+        'spend_location': 'some file path',
+        'spend_partitions': 2,
+        'signups_location': 'some file path',
+        'signups_partitions': 2
+    }
+    dr = driver.Driver(initial_config_and_data, *modules, adapter=dga)
+
+    output_columns = [
+        'spend',
+        'signups',
+        'avg_3wk_spend',
+        'spend_per_signup',
+        'spend_zero_mean_unit_variance'
+    ]
+    df = dr.execute(output_columns, display_graph=True)
+    # will output test-output/execute.gv.pdf
+    logger.info(df.to_string())
+    client.shutdown()
---
+++ hamilton/experimental/h_dask.py
@@ -0,0 +1,71 @@
+import logging
+import typing
+
+import pandas as pd
+import numpy as np
+
+from dask import compute
+from dask.delayed import Delayed, delayed
+from dask.distributed import Client as DaskClient
+import dask.dataframe
+import dask.array
+
+
+from hamilton import node
+from hamilton import base
+
+
+logger = logging.getLogger(__name__)
+
+
+class DaskGraphAdapter(base.HamiltonGraphAdapter):
+    """Class representing what's required to make Hamilton run on Dask
+
+    DISCLAIMER -- this class is experimental, so signature changes are a possibility!
+    """
+
+    def __init__(self, dask_client: DaskClient, result_builder: base.ResultMixin = None, visualize: bool = True):
+        """Constructor
+
+        :param dask_client: the dask client -- we don't do anything with it, but thought that it would be useful
+            to wire through here.
+        :param result_builder: The function that will build the result. Optional, defaults to pandas dataframe.
+        :param visualize: whether we want to visualize what Dask wants to execute.
+        """
+        self.client = dask_client
+        self.result_builder = result_builder if result_builder else base.PandasDataFrameResult()
+        self.visualize = visualize
+
+    @staticmethod
+    def check_input_type(node_type: typing.Type, input_value: typing.Any) -> bool:
+        # NOTE: the type of dask Delayed is unknown until they are computed
+        if isinstance(input_value, Delayed):
+            return True
+        elif node_type == pd.Series and isinstance(input_value, dask.dataframe.Series):
+            return True
+        elif node_type == np.array and isinstance(input_value, dask.array.Array):
+            return True
+        return node_type == typing.Any or isinstance(input_value, node_type)
+
+    @staticmethod
+    def check_node_type_equivalence(node_type: typing.Type, input_type: typing.Type) -> bool:
+        if node_type == dask.array.Array and input_type == pd.Series:
+            return True
+        elif node_type == dask.dataframe.Series and input_type == pd.Series:
+            return True
+        return node_type == input_type
+
+    def execute_node(self, node: node.Node, kwargs: typing.Dict[str, typing.Any]) -> typing.Any:
+        return delayed(node.callable)(**kwargs)
+
+    def build_result(self, **columns: typing.Dict[str, typing.Any]) -> pd.DataFrame:
+        for k, v in columns.items():
+            logger.info(f'Got column {k}, with type [{type(v)}].')
+        delayed_combine = delayed(self.result_builder.build_result)(**columns)
+        if self.visualize:
+            delayed_combine.visualize()
+        df, = compute(delayed_combine)
+        return df
+
+
+# TODO: add ResultMixins for dask types
