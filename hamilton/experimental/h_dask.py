4b0ff270f22d66ff7264cc732e2d7a48598083d0
skrawczyk <skrawczyk@stitchfix.com>
2022-02-04 15:08:29 -0800

    Adds [Ray|Dask|Koalas]GraphAdapter documentation
    It adds information on scaling characteristics, and pointers to
    documentation where it makes sense.
    
    I guess over time we'll add more to these classes as we get
    feedback about what works, and what doesn't.

--- hamilton/experimental/h_dask.py
+++ hamilton/experimental/h_dask.py
@@ -21,6 +21,35 @@ logger = logging.getLogger(__name__)
 class DaskGraphAdapter(base.HamiltonGraphAdapter):
     """Class representing what's required to make Hamilton run on Dask
 
+    Use `pip install sf-hamilton[dask]` to get the dependencies required to run this.
+
+    Dask is a good choice to scale computation when you really can't do things in memory anymore with pandas. For
+    most simple pandas operations, you should not have to do anything to scale!
+
+    Dask is also a good choice if you want to scale computation generally -- you'll just have to switch to natively
+    using their object types if that's the case.
+
+    Please read the following notes about it:
+
+    # Notes on scaling:
+      - Multi-core on single machine ✅
+      - Distributed computation on a Dask cluster ✅
+      - Scales to any size of data supported by Dask ✅; assuming you load it appropriately via Dask loaders.
+
+    # Function return object types supported:
+     - Works for any python object that can be serialized by the Dask framework. ✅
+
+    # Pandas?
+     - Dask implements a good subset of the Pandas API:
+       > You might be able to get away with scaling without having to change your code at all!
+       > See https://docs.dask.org/en/latest/dataframe-api.html for Pandas supported APIs.
+       > If it not supported by their API, you have to then read up and think about how to structure you hamilton
+       > function computation -- https://docs.dask.org/en/latest/dataframe.html
+
+    # Loading Data:
+     - see https://docs.dask.org/en/latest/best-practices.html#load-data-with-dask
+     - we recommend creating a python module specifically encapsulating functions that help you load data.
+
     DISCLAIMER -- this class is experimental, so signature changes are a possibility!
     """
 
@@ -56,11 +85,23 @@ class DaskGraphAdapter(base.HamiltonGraphAdapter):
         return node_type == input_type
 
     def execute_node(self, node: node.Node, kwargs: typing.Dict[str, typing.Any]) -> typing.Any:
+        """Function that is called as we walk the graph to determine how to execute a hamilton function.
+
+        :param node: the node from the graph.
+        :param kwargs: the arguments that should be passed to it.
+        :return: returns a dask delayed object.
+        """
         return delayed(node.callable)(**kwargs)
 
     def build_result(self, **columns: typing.Dict[str, typing.Any]) -> typing.Any:
-        for k, v in columns.items():
-            logger.info(f'Got column {k}, with type [{type(v)}].')
+        """Builds the result and brings it back to this running process.
+
+        :param columns: the dictionary of key -> Union[delayed object reference | value]
+        :return: The type of object returned by self.result_builder.
+        """
+        if logger.isEnabledFor(logging.DEBUG):
+            for k, v in columns.items():
+                logger.info(f'Got column {k}, with type [{type(v)}].')
         delayed_combine = delayed(self.result_builder.build_result)(**columns)
         if self.visualize:
             delayed_combine.visualize()
--- hamilton/experimental/h_spark.py
+++ hamilton/experimental/h_spark.py
@@ -17,18 +17,35 @@ class KoalasDataFrameResult(base.ResultMixin):
     """Mixin for building a koalas dataframe from the result"""
 
     @staticmethod
-    def build_result(**columns: typing.Dict[str, typing.Any]) -> pd.DataFrame:
-        """TODO figure out how to best signal that we want a koalas DF result."""
+    def build_result(**columns: typing.Dict[str, typing.Any]) -> ps.DataFrame:
+        """Right now this class is just used for signaling the return type."""
         pass
 
 
 class SparkKoalasGraphAdapter(base.HamiltonGraphAdapter, base.ResultMixin):
     """Class representing what's required to make Hamilton run on Spark with Koalas.
 
+    Use `pip install sf-hamilton[spark]` to get the dependencies required to run this.
+
     Currently this class assumes you're running SPARK 3.2+.
 
     Some tips on koalas (before it was merged into spark 3.2):
      - https://databricks.com/blog/2020/03/31/10-minutes-from-pandas-to-koalas-on-apache-spark.html
+     - https://spark.apache.org/docs/latest/api/python/user_guide/pandas_on_spark/index.html
+
+    Spark is a more heavyweight choice to scale computation for Hamilton graphs creating a Pandas Dataframe.
+
+    # Notes on scaling:
+      - Multi-core on single machine ✅ (if you setup Spark locally to do so)
+      - Distributed computation on a Spark cluster ✅
+      - Scales to any size of data as permitted by Spark ✅
+
+    # Function return object types supported:
+     - ⛔ Not generic. This does not work for every Hamilton graph.
+     - ✅ Currently we're targeting this at Pandas/Koalas types [dataframes, series].
+
+    # Pandas?
+     - ✅ Koalas on Spark 3.2+ implements a good subset of the pandas API. Keep it simple and you should be good to go!
 
     DISCLAIMER -- this class is experimental, so signature changes are a possibility!
     """
@@ -50,6 +67,14 @@ class SparkKoalasGraphAdapter(base.HamiltonGraphAdapter, base.ResultMixin):
 
     @staticmethod
     def check_input_type(node_type: typing.Type, input_value: typing.Any) -> bool:
+        """Function to equate an input value, with expected node type.
+
+        We need this to equate pandas and koalas objects/types.
+
+        :param node_type: the declared node type
+        :param input_value: the actual input value
+        :return: whether this is okay, or not.
+        """
         # TODO: flesh this out more
         if (node_type == pd.Series or node_type == ps.Series) and (
                 isinstance(input_value, ps.DataFrame) or isinstance(input_value, ps.Series)):
@@ -61,6 +86,12 @@ class SparkKoalasGraphAdapter(base.HamiltonGraphAdapter, base.ResultMixin):
 
     @staticmethod
     def check_node_type_equivalence(node_type: typing.Type, input_type: typing.Type) -> bool:
+        """Function to help equate pandas with koalas types.
+
+        :param node_type: the declared node type.
+        :param input_type: the type of what we want to pass into it.
+        :return: whether this is okay, or not.
+        """
         if node_type == ps.Series and input_type == pd.Series:
             return True
         elif node_type == pd.Series and input_type == ps.Series:
@@ -72,6 +103,12 @@ class SparkKoalasGraphAdapter(base.HamiltonGraphAdapter, base.ResultMixin):
         return node_type == input_type
 
     def execute_node(self, node: node.Node, kwargs: typing.Dict[str, typing.Any]) -> typing.Any:
+        """Function that is called as we walk the graph to determine how to execute a hamilton function.
+
+        :param node: the node from the graph.
+        :param kwargs: the arguments that should be passed to it.
+        :return: returns a koalas column
+        """
         return node.callable(**kwargs)
 
     def build_result(self, **columns: typing.Dict[str, typing.Any]) -> typing.Union[pd.DataFrame, ps.DataFrame]:
