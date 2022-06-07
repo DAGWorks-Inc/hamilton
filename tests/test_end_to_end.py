8e0f4a4f92329cea74311f3e71afb0135b8bc50c
elijahbenizzy <elijah_ben_izzy@alumni.brown.edu>
2022-06-07 16:24:22 -0700

    Adds end-to-end tests for data quality

---
+++ tests/test_end_to_end.py
@@ -0,0 +1,26 @@
+import pytest
+
+import hamilton.driver
+from hamilton import graph
+import tests.resources.data_quality
+from hamilton.data_quality.base import ValidationResult, DataValidationError
+
+
+def test_data_quality_workflow_passes():
+    driver = hamilton.driver.Driver({}, tests.resources.data_quality)
+    all_vars = driver.list_available_variables()
+    result = driver.raw_execute([var.name for var in all_vars], inputs={'data_quality_should_fail': False})
+    dq_nodes = [var.name for var in all_vars if var.tags.get('hamilton.data_quality.contains_dq_results', False)]
+    assert len(dq_nodes) == 1
+    dq_result = result[dq_nodes[0]]
+    assert isinstance(dq_result, ValidationResult)
+    assert dq_result.passes is True
+
+
+def test_data_quality_workflow_fails():
+    driver = hamilton.driver.Driver({}, tests.resources.data_quality)
+    all_vars = driver.list_available_variables()
+    with pytest.raises(DataValidationError):
+        driver.raw_execute([var.name for var in all_vars], inputs={'data_quality_should_fail': True})
+
+
