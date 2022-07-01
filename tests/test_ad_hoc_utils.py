da015bc53df5424c6de6a114207fa9e785240d96
Stefan Krawczyk <skrawczyk@stitchfix.com>
2022-07-01 16:31:22 -0700

    Adds the ability to pass functions not defined in modules - fixes #134 (#145)
    We went with the decision to create an object to house functions for three reasons:
    
    1. It meant we did not need to structurally change the underlying code, other than checking
    for the object instance type, and amending some type annotations.
    2. This adds some friction to force people to at least acknowledge they need to curate the functions.
    The name `TemporaryFunctionModule` should indicate that this should not be a production thing.
    3. This opens the door for us to change our mind in the future about how we do this.
    
    The `my_notebook_script.py` shows what one would do in a notebook context,
    i.e. shows how you can inline define functions that interact with a module that constructs a DAG.
    
    Assumption we did not break:
    
     - you cannot define the same function twice -- so you cannot use this as a way to override a  previously
    defined function.
    
    Adding this for context, if you want to overwrite a function implementation you instead have the following paths:
    
    1. Choose the subset of functions you want and put them in the temporary module, i.e. import those functions.
    2. (a)Run the upstream part of the DAG to create the inputs required for the new function.
    (b) Then compute the result with the new function. (c) Then run the rest of the DAG using the `override=` option of execute.
    3. Or, alternatively, import the module, and overwrite the function definition you want to change (caveats apply!). Then continue as normal with Hamilton.
    
    Usage example -- not showing imports:
    ```python
    def signups() -> pd.Series:
        """Returns sign up values"""
        return pd.Series([1, 10, 50, 100, 200, 400])
    
    def spend() -> pd.Series:
        """Returns the spend values"""
        return pd.Series([10, 10, 20, 40, 40, 50])
    
    def log_spend_per_signup(spend_per_signup: pd.Series) -> pd.Series:
        """Simple function taking the logarithm of spend over signups."""
        return np.log(spend_per_signup)
    
    temp_module = ad_hoc_utils.create_temporary_module(spend, signups, log_spend_per_signup, module_name='function_example')
    
    initial_config = {}
    dr = driver.Driver(initial_config, my_functions, temp_module)  # can pass in multiple modules
    output_columns = [
        'spend',
        'signups',
        'avg_3wk_spend',
        'spend_per_signup',
        'spend_zero_mean_unit_variance',
        'log_spend_per_signup'
    ]
    df = dr.execute(output_columns)
    ```
    
    Co-authored-by: elijahbenizzy <elijah_ben_izzy@alumni.brown.edu>

---
+++ tests/test_ad_hoc_utils.py
@@ -0,0 +1,46 @@
+import inspect
+
+from hamilton import ad_hoc_utils
+from hamilton import function_modifiers
+
+
+def test_copy_func():
+    """Tests that we copy the function as intended"""
+    @function_modifiers.tag(test_function='true')
+    def foo(bar: int) -> int:
+        """dummy function"""
+        return bar + 1
+
+    cloned_func = ad_hoc_utils._copy_func(foo)
+
+    assert cloned_func.__dict__ == foo.__dict__
+    assert cloned_func.__annotations__ == foo.__annotations__
+
+    assert cloned_func is not foo
+    assert cloned_func(1) == foo(1)
+
+
+def test_generate_unique_temp_module_name():
+    """Tests that we replace - with _"""
+    name = ad_hoc_utils._generate_unique_temp_module_name()
+    assert '-' not in name
+
+
+def test_create_temporary_module():
+    """Tests that we create a module with the passed in functions."""
+
+    def bar(baz: int) -> int:
+        """dummy function"""
+        return baz + 1
+
+    def foo(bar: int) -> int:
+        """dummy function"""
+        return bar + 1
+
+    temp_module = ad_hoc_utils.create_temporary_module(bar, foo)
+    expected_members = {'__spec__', '__loader__', '__name__', '__doc__', 'bar', 'foo', '__package__'}
+    assert set(dict(inspect.getmembers(temp_module)).keys()) == expected_members
+    assert '_' in temp_module.__name__
+    temp_module_2 = ad_hoc_utils.create_temporary_module(bar, foo, module_name='test_module')
+    assert set(dict(inspect.getmembers(temp_module_2)).keys()) == expected_members
+    assert temp_module_2.__name__ == 'test_module'
--- tests/test_graph.py
+++ tests/test_graph.py
@@ -19,7 +19,7 @@ import tests.resources.optional_dependencies
 import tests.resources.parametrized_inputs
 import tests.resources.parametrized_nodes
 import tests.resources.typing_vs_not_typing
-from hamilton import graph, base
+from hamilton import graph, base, ad_hoc_utils
 from hamilton import node
 from hamilton.node import NodeSource
 
@@ -32,6 +32,20 @@ def test_find_functions():
     actual = graph.find_functions(tests.resources.dummy_functions)
     assert len(actual) == len(expected)
     assert actual == expected
+
+
+def test_find_functions_from_temporary_function_module():
+    """Tests that we handle the TemporaryFunctionModule object correctly."""
+    expected = [('A', tests.resources.dummy_functions.A),
+                ('B', tests.resources.dummy_functions.B),
+                ('C', tests.resources.dummy_functions.C)]
+    func_module = ad_hoc_utils.create_temporary_module(tests.resources.dummy_functions.A,
+                                               tests.resources.dummy_functions.B,
+                                               tests.resources.dummy_functions.C)
+    actual = graph.find_functions(func_module)
+    assert len(actual) == len(expected)
+    assert [node_name for node_name, _ in actual] == [node_name for node_name, _ in expected]
+    assert [fn.__code__ for _, fn in actual] == [fn.__code__ for _, fn in expected] # easy way to say they're the same
 
 
 def test_add_dependency_missing_param_type():
@@ -646,3 +660,14 @@ def test_optional_get_required_runtime_with_provided():
     all_upstream, user_required = fg.get_upstream_nodes(['g'], runtime_inputs={'b': 109})  # Nothng required
     assert len(all_upstream) == 4  # 6 total nodes upstream
     assert len(user_required) == 1  # 4 nodes required input
+
+
+def test_in_driver_function_definitions():
+    """Tests that we can instantiate a DAG with a function defined in the driver, e.g. notebook context"""
+    def my_function(A: int, b: int, c: int) -> int:
+        """Function for input below"""
+        return A + b + c
+    f_module = ad_hoc_utils.create_temporary_module(my_function)
+    fg = graph.FunctionGraph(tests.resources.dummy_functions, f_module, config={'b': 3, 'c': 1})
+    results = fg.execute([n for n in fg.get_nodes() if n.name in ['my_function', 'A']])
+    assert results == {'A': 4, 'b': 3, 'c': 1, 'my_function': 8}
