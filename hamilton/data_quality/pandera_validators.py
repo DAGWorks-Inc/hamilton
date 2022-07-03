8568fe8146a0877d912a6194691ae9d138636a7b
elijahbenizzy <elijah_ben_izzy@alumni.brown.edu>
2022-07-02 17:00:14 -0700

    Adds pandera integration for data quality
    This is a pretty simple approach but I think it works nicely.
    
    We use the schema= decorator to specify data quality checks
    that validate a pandera schema. Note that this will only be registered
    if pandera is installed (which is an option for setup.py).
    
    The other option we were thinking of is to compile the default checks to
    pandera, but I think that's a little overkill, and can be done later.
    
    Furthermore, this proves out the abstraction for data validation quite
    nicely -- this was straightforward to implement.

---
+++ hamilton/data_quality/pandera_validators.py
@@ -0,0 +1,84 @@
+from typing import Any, Type
+
+import pandas as pd
+import pandera as pa
+
+from hamilton.data_quality import base
+from hamilton.data_quality.default_validators import BaseDefaultValidator
+
+
+class PanderaDataFrameValidator(BaseDefaultValidator):
+    """Pandera schema validator for dataframes"""
+
+    def __init__(self, schema: pa.DataFrameSchema):
+        self.schema = schema
+
+    @classmethod
+    def applies_to(cls, datatype: Type[Type]) -> bool:
+        return issubclass(datatype, pd.DataFrame)  # TODO -- allow for modin, etc. as they come for free with pandera
+
+    def description(self) -> str:
+        return f"Validates that the returned dataframe matches the pander"
+
+    def validate(self, data: pd.DataFrame) -> base.ValidationResult:
+        try:
+            self.schema.validate(data, lazy=True)
+        except pa.errors.SchemaErrors as e:
+            return base.ValidationResult(
+                passes=False,
+                message=str(e),
+                diagnostics={'schema_errors': e.schema_errors}
+            )
+        return base.ValidationResult(
+            passes=True,
+            message=f"Data passes pandera check for schema {str(self.schema)}"
+            # TDOO -- add diagnostics data with serialized the schema
+        )
+
+    @classmethod
+    def arg(cls) -> str:
+        return "schema"  # TODO -- determine whether we want to allow other schemas
+
+    @classmethod
+    def name(cls) -> str:
+        return "pandera_schema_validator"
+
+
+class PanderaSeriesSchemaValidator(BaseDefaultValidator):
+    """Pandera schema validator for series"""
+
+    def __init__(self, schema: pa.SeriesSchema):
+        self.schema = schema
+
+    @classmethod
+    def applies_to(cls, datatype: Type[Type]) -> bool:
+        return issubclass(datatype, pd.Series)  # TODO -- allow for modin, etc. as they come for free with pandera
+
+    def description(self) -> str:
+        pass
+
+    def validate(self, data: pd.Series) -> base.ValidationResult:
+        try:
+            self.schema.validate(data, lazy=True)
+        except pa.errors.SchemaErrors as e:
+            return base.ValidationResult(
+                passes=False,
+                message=str(e),
+                diagnostics={'schema_errors': e.schema_errors}
+            )
+        return base.ValidationResult(
+            passes=True,
+            message=f"Data passes pandera check for schema {str(self.schema)}"
+            # TDOO -- add diagnostics data with serialized the schema
+        )
+
+    @classmethod
+    def arg(cls) -> str:
+        return "schema"  # TODO -- determine whether we want to allow other schemas
+
+    @classmethod
+    def name(cls) -> str:
+        return "pandera_schema_validator"
+
+
+PANDERA_VALIDATORS = [PanderaDataFrameValidator, PanderaSeriesSchemaValidator]
