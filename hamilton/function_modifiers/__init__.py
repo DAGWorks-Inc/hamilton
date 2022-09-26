724e4e2dc825ec0c2a500f62c92abcac2c54aa1e
elijahbenizzy <elijah_ben_izzy@alumni.brown.edu>
2022-09-25 18:02:22 -0700

    Implements the reuse_subdag operator

--- hamilton/function_modifiers/__init__.py
+++ hamilton/function_modifiers/__init__.py
@@ -4,7 +4,7 @@ from typing import Any, Callable, Collection, Dict, List, Type
 
 from hamilton import node
 
-from . import base, configuration, dependencies, expanders, macros, metadata, validation
+from . import base, configuration, dependencies, expanders, macros, metadata, reuse, validation
 
 logger = logging.getLogger(__name__)
 
@@ -62,3 +62,7 @@ check_output = validation.check_output
 check_output_custom = validation.check_output_custom
 IS_DATA_VALIDATOR_TAG = validation.IS_DATA_VALIDATOR_TAG
 DATA_VALIDATOR_ORIGINAL_OUTPUT_TAG = validation.DATA_VALIDATOR_ORIGINAL_OUTPUT_TAG
+
+# reuse-subdag
+
+reuse_functions = reuse.reuse_subdag
