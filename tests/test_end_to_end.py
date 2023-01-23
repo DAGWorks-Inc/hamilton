d98991fed5185113a4348324c12d363ab41012bf
elijahbenizzy <elijah_ben_izzy@alumni.brown.edu>
2023-01-23 08:43:11 -0800

    Tests out user-reported bug
    So far cannot reproduce... Makes me wonder if there's something simple
    I'm missing, or if the other modules brings in another set of
    functions...

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -1,10 +1,13 @@
 import sys
 
+import pandas as pd
 import pytest
 
 import hamilton.driver
 import tests.resources.data_quality
 import tests.resources.smoke_screen_module
+from hamilton import ad_hoc_utils, base
+from hamilton.base import SimplePythonGraphAdapter
 from hamilton.data_quality.base import DataValidationError, ValidationResult
 
 
@@ -55,3 +58,39 @@ def test_smoke_screen_module():
     assert abs(df.mean()["neutral_net_acquisition_cost"] - 0.405582) < epsilon
     assert abs(df.mean()["optimistic_net_acquisition_cost"] - 0.399363) < epsilon
     assert df["series_with_start_date_end_date"].iloc[0] == "date_20200101_date_20220801"
+
+
+def test_configs_work_as_inputs():
+    # Taken from user-reported bug:
+    # https://hamilton-opensource.slack.com/archives/C03M33QB4M8
+    # /p1674466068367019
+
+    def load_sat_data(satellite_product: str) -> pd.DataFrame:
+        return pd.DataFrame.from_records([{satellite_product: 1}])
+
+    config = {"satellite_product": "sm"}
+    dr = hamilton.driver.Driver(
+        config,
+        ad_hoc_utils.create_temporary_module(load_sat_data),
+        adapter=SimplePythonGraphAdapter(base.DictResult()),
+    )
+    results = dr.execute(["load_sat_data"])
+    pd.testing.assert_frame_equal(results["load_sat_data"], pd.DataFrame.from_records([{"sm": 1}]))
+
+
+def test_inputs_work_as_inputs():
+    # Taken from user-reported bug:
+    # https://hamilton-opensource.slack.com/archives/C03M33QB4M8
+    # /p1674466068367019
+
+    def load_sat_data(satellite_product: str) -> pd.DataFrame:
+        return pd.DataFrame.from_records([{satellite_product: 1}])
+
+    dr = hamilton.driver.Driver(
+        {},
+        ad_hoc_utils.create_temporary_module(load_sat_data),
+        adapter=SimplePythonGraphAdapter(base.DictResult()),
+    )
+    inputs = {"satellite_product": "sm"}
+    results = dr.execute(["load_sat_data"], inputs=inputs)
+    pd.testing.assert_frame_equal(results["load_sat_data"], pd.DataFrame.from_records([{"sm": 1}]))
