69732a94a92e91293948f7a831624117a45f1d52
Stefan Krawczyk <stefan@dagworks.io>
2023-03-03 21:19:38 -0800

    Adds feature engineering scenario 2 example
    This example here is contrived again. However it should illustrate how you can replace
    getting data with Hamilton quite easily. The example wont fit the needs of everyone, since
    people's needs will likely fall in between scenario 1 and scenario 2, but hopefully it provides
    them enough context to get going with feature engineering and Hamilton.

---
+++ examples/feature_engineering_multiple_contexts/scenario_2/named_model_feature_sets.py
@@ -0,0 +1,17 @@
+"""
+This module is a light weight way to "record" feature sets that are used in a model; just define it in code!
+
+No need for a database! Just use code and `git` to version what is or isn't used/available.
+"""
+
+model_x_features = [
+    "is_summer",
+    "has_pet",
+    "has_children",
+    "day_of_the_week_2",
+    "day_of_the_week_3",
+    "day_of_the_week_4",
+    "day_of_the_week_5",
+    "day_of_the_week_6",
+    "age_zero_mean_unit_variance",
+]
