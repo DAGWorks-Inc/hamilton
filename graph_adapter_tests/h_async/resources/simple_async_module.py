0857e5d20eacfbc4eb528d40ecc2a0a31b21e4bc
Stefan Krawczyk <stefan@dagworks.io>
2023-03-03 20:40:54 -0800

    Fixes #93
    This is a less than ideal solution, but basically if the function being wrapped is a coroutine,
    then make the wrapper async as well. This duplicates a lot of code. I'm sure there's
    a more succinct way to do this, but because I'm time pressed I'm doing the more
    verbose solution.
    
    Note: this doesn't fix it for all decorators, just the extract* ones.
    
    Adds async tests to double check and ensure that things work as expected.

--- graph_adapter_tests/h_async/resources/simple_async_module.py
+++ graph_adapter_tests/h_async/resources/simple_async_module.py
@@ -1,6 +1,8 @@
 import asyncio
 from typing import Dict
 
+import pandas as pd
+
 import hamilton.function_modifiers
 
 
@@ -28,3 +30,13 @@ def non_async_func_with_decorator(
     async_func_with_param: int, another_async_func: int
 ) -> Dict[str, int]:
     return {"result_1": another_async_func + 1, "result_2": async_func_with_param + 1}
+
+
+@hamilton.function_modifiers.extract_columns(*["a", "b"])
+async def return_df() -> pd.DataFrame:
+    return pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+
+
+@hamilton.function_modifiers.extract_fields(dict(result_3=int, result_4=int))
+async def return_dict() -> dict:
+    return {"result_3": 1, "result_4": 2}
