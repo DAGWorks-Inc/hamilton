bcc298247b3de98a2accf10646d8e07b0d9720fb
elijahbenizzy <elijah_ben_izzy@alumni.brown.edu>
2023-03-08 21:29:14 -0800

    Adds group() dependency type
    A few things we need to iron out, but this allows group() in addition to
    @source and @value.

--- hamilton/function_modifiers/dependencies.py
+++ hamilton/function_modifiers/dependencies.py
@@ -1,7 +1,10 @@
 import abc
 import dataclasses
 import enum
-from typing import Any
+import typing
+from typing import Any, List, Sequence, Type
+
+import typing_inspect
 
 """Utilities for specifying dependencies/dependency types in other decorators."""
 
@@ -9,12 +12,17 @@ from typing import Any
 class ParametrizedDependencySource(enum.Enum):
     LITERAL = "literal"
     UPSTREAM = "upstream"
+    GROUPED_LIST = "grouped_list"
 
 
 class ParametrizedDependency:
     @abc.abstractmethod
     def get_dependency_type(self) -> ParametrizedDependencySource:
         pass
+
+
+class SingleDependency(ParametrizedDependency, abc.ABC):
+    pass
 
 
 @dataclasses.dataclass
@@ -31,6 +39,32 @@ class UpstreamDependency(ParametrizedDependency):
 
     def get_dependency_type(self) -> ParametrizedDependencySource:
         return ParametrizedDependencySource.UPSTREAM
+
+
+@dataclasses.dataclass
+class GroupedListDependency(ParametrizedDependency):
+    @staticmethod
+    def get_type(containing_type: Type[Sequence[Type]], param_name: str):
+        origin = typing_inspect.get_origin(containing_type)
+        if origin is None or not issubclass(origin, typing.Sequence):
+            raise ValueError(
+                f"Type: {containing_type} for parameter: {param_name} needs to be "
+                f"sequence to use the group() dependency specification. Otherwise hamilton"
+                f"cannot validate that the types are correct."
+            )
+        args = typing_inspect.get_args(containing_type)
+        if not len(args) == 1:
+            raise ValueError(
+                f"Type: {containing_type} for parameter: {param_name} needs to be "
+                f"sequence with one type argument to use the group() dependency specification. "
+                f"Otherwise Hamilton cannot validate that the types are correct."
+            )
+        return args[0]
+
+    def get_dependency_type(self) -> ParametrizedDependencySource:
+        return ParametrizedDependencySource.GROUPED_LIST
+
+    sources: List[ParametrizedDependency]
 
 
 def value(literal_value: Any) -> LiteralDependency:
@@ -58,3 +92,19 @@ def source(dependency_on: Any) -> UpstreamDependency:
     if isinstance(dependency_on, UpstreamDependency):
         return dependency_on
     return UpstreamDependency(source=dependency_on)
+
+
+def group(*dependencies: ParametrizedDependency) -> GroupedListDependency:
+    """Specifies that a parameterized dependency comes from a "grouped" source.
+
+    This means that it gets injected into a list of dependencies that are grouped together. E.G.
+    dep=group(source("foo"), source("bar")) for the function:
+
+    def f(dep: List[pd.Series]) -> pd.Series:
+        return ...
+
+    Would result in dep getting foo and bar dependencies injected.
+    :param dependencies: Dependencies, list of dependencies
+    :return:
+    """
+    return GroupedListDependency(sources=list(dependencies))
