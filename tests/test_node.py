6434849c878f187d8bf74946971635e8b413c571
elijahbenizzy <elijah_ben_izzy@alumni.brown.edu>
2023-07-01 17:02:06 -0700

    Adds originating_functions to copy_to
    This was getting lost, so the originating functions weren't getting
    bound to the node when copying.

--- tests/test_node.py
+++ tests/test_node.py
@@ -20,3 +20,16 @@ def test_node_from_fn_sad_no_type_hint():
 
     with pytest.raises(ValueError):
         Node.from_fn(fn)
+
+
+def test_node_copy_with_retains_originating_functions():
+    def fn() -> int:
+        return 1
+
+    node = Node.from_fn(fn)
+    node_copy = node.copy_with(name="rename_fn", originating_functions=(fn,))
+    assert node_copy.originating_functions == (fn,)
+    assert node_copy.name == "rename_fn"
+    node_copy_copy = node_copy.copy_with(name="rename_fn_again")
+    assert node_copy_copy.originating_functions == (fn,)
+    assert node_copy_copy.name == "rename_fn_again"
