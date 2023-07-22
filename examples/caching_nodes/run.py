30f42571a5a1390ef5fa95a57f02bf904ab2dda9
Michal Siedlaczek <michal@siedlaczek.me>
2023-07-21 21:16:18 -0400

    Fix caching adapter example

--- examples/caching_nodes/run.py
+++ examples/caching_nodes/run.py
@@ -1,17 +1,22 @@
 import logging
+import pathlib
 import sys
 
-from hamilton import driver
-
-import data_loaders
 import business_logic
+import data_loaders
+
+from hamilton import base, driver
+from hamilton.experimental import h_cache
 
 logging.basicConfig(stream=sys.stdout)
 
 # This is empty, we get the data from the data_loaders module
 initial_columns = {}
 
-dr = driver.Driver(initial_columns, business_logic, data_loaders)
+cache_path = "tmp"
+pathlib.Path(cache_path).mkdir(exist_ok=True)
+adapter = h_cache.CachingGraphAdapter(cache_path, base.PandasDataFrameResult())
+dr = driver.Driver(initial_columns, business_logic, data_loaders, adapter=adapter)
 output_columns = [
     data_loaders.spend,
     data_loaders.signups,
