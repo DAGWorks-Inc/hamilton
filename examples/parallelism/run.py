b895de2e69335c61d14f07daeca2dbd16c8e2796
elijahbenizzy <elijah_ben_izzy@alumni.brown.edu>
2023-07-28 10:27:36 -0700

    Adds dask/ray task-based execution
    Still experimental, so we're likely to change the API. Mostly add new
    parameters, its build to be backwards compatible.

--- examples/parallelism/run.py
+++ examples/parallelism/run.py
@@ -1,21 +1,55 @@
+import logging
+
 import aggregate_data
+import click
 import list_data
 import process_data
+import ray
+from dask import distributed
 
-from hamilton import driver
+from hamilton import driver, log_setup
 from hamilton.execution import executors
+from hamilton.experimental import h_dask, h_ray
+
+log_setup.setup_logging(logging.INFO)
 
 
-def main():
+@click.command()
+@click.option(
+    "--mode",
+    type=click.Choice(["local", "multithreading", "dask", "ray"]),
+    required=True,
+    help="Where to run remote tasks.",
+)
+def main(mode: str):
+    shutdown = None
+    if mode == "local":
+        remote_executor = executors.SynchronousLocalTaskExecutor()
+    elif mode == "multithreading":
+        remote_executor = executors.MultiThreadingExecutor(max_tasks=100)
+    elif mode == "dask":
+        cluster = distributed.LocalCluster()
+        client = distributed.Client(cluster)
+        remote_executor = h_dask.DaskExecutor(client=client)
+        shutdown = cluster.close
+    else:
+        remote_executor = h_ray.RayTaskExecutor(num_cpus=4)
+        shutdown = ray.shutdown
     dr = (
         driver.Builder()
         .enable_v2_driver(allow_experimental_mode=True)
-        .with_remote_executor(executors.MultiThreadingExecutor(max_tasks=100))
-        .with_local_executor(executors.SynchronousLocalTaskExecutor())
+        .with_remote_executor(remote_executor)  # We only need to specify remote exeecutor
+        # The local executor just runs it synchronously
         .with_modules(aggregate_data, list_data, process_data)
         .build()
     )
-    print(dr.execute(final_vars=["statistics_by_city"], inputs={"data_dir": "data"}))
+    print(
+        dr.execute(final_vars=["statistics_by_city"], inputs={"data_dir": "data"})[
+            "statistics_by_city"
+        ]
+    )
+    if shutdown:
+        shutdown()
 
 
 if __name__ == "__main__":
