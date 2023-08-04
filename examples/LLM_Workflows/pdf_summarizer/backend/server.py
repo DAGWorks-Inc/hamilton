b5c4a9915f81f48366098e2a11bc6b5ce2c96362
Thierry Jean <68975210+zilto@users.noreply.github.com>
2023-08-04 01:11:56 -0400

    Created a PDF summarizer example using fastapi and streamlit (#250)
    Creates a PDF summarizer example, that is hosted via fastapi with a streamlit frontend.
    
    This example shows one how you can use Hamilton in a FastAPI webservice that talks to streamlit.
    
    The Hamilton part orchestrates taking the request, and running it through a dataflow that summarizes a PDF.
    
    Stay tuned for follow ups: 
     - e.g. debugging
     - e.g. DAGWorks tracking
    
    ---------
    
    Co-authored-by: zilto <tjean@DESKTOP-V6JDCS2.localdomain>
    Co-authored-by: Stefan Krawczyk <stefan@dagworks.io>

---
+++ examples/LLM_Workflows/pdf_summarizer/backend/server.py
@@ -0,0 +1,94 @@
+import base64
+
+import fastapi
+import pydantic
+import summarization
+
+from hamilton import base, driver
+from hamilton.experimental import h_async
+
+# instantiate FastAPI app
+app = fastapi.FastAPI()
+
+
+# define constants for Hamilton driver
+driver_config = dict(
+    file_type="pdf",
+)
+
+# instantiate the Hamilton driver; it will power all API endpoints
+# async driver for use with async functions
+async_dr = h_async.AsyncDriver(
+    driver_config,
+    summarization,  # python module containing function logic
+    result_builder=base.DictResult(),
+)
+# sync driver for use with regular functions
+sync_dr = driver.Driver(
+    driver_config,
+    summarization,  # python module containing function logic
+    adapter=base.SimplePythonGraphAdapter(base.DictResult()),
+)
+
+
+class SummarizeResponse(pydantic.BaseModel):
+    """Response to the /summarize endpoint"""
+
+    summary: str
+
+
+@app.post("/summarize")
+async def summarize_pdf(
+    pdf_file: fastapi.UploadFile,
+    openai_gpt_model: str = "gpt-3.5-turbo-0613",
+    content_type: str = "Scientific article",
+    user_query: str = "Can you ELI5 the paper?",
+) -> SummarizeResponse:
+    """Request `summarized_text` from Hamilton driver with `pdf_file` and `user_query`"""
+    results = await async_dr.execute(
+        ["summarized_text"],
+        inputs=dict(
+            pdf_source=pdf_file.file,
+            openai_gpt_model=openai_gpt_model,
+            content_type=content_type,
+            user_query=user_query,
+        ),
+    )
+
+    return SummarizeResponse(summary=results["summarized_text"])
+
+
+@app.post("/summarize_sync")
+def summarize_pdf_sync(
+    pdf_file: fastapi.UploadFile,
+    openai_gpt_model: str = "gpt-3.5-turbo-0613",
+    content_type: str = "Scientific article",
+    user_query: str = "Can you ELI5 the paper?",
+) -> SummarizeResponse:
+    """Request `summarized_text` from Hamilton driver with `pdf_file` and `user_query`"""
+    results = sync_dr.execute(
+        ["summarized_text"],
+        inputs=dict(
+            pdf_source=pdf_file.file,
+            openai_gpt_model=openai_gpt_model,
+            content_type=content_type,
+            user_query=user_query,
+        ),
+    )
+
+    return SummarizeResponse(summary=results["summarized_text"])
+
+
+# add to SwaggerUI the execution DAG png
+# see http://localhost:8080/docs#/default/summarize_pdf_summarize_post
+base64_viz = base64.b64encode(open("summarize_route.png", "rb").read()).decode("utf-8")
+app.routes[
+    -1
+].description = f"""<h1>Execution DAG</h1><img alt="" src="data:image/png;base64,{base64_viz}"/>"""
+
+
+if __name__ == "__main__":
+    # run as a script to test server locally
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8080)
