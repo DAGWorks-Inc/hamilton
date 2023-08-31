d8dc4bed06b38f9ded4595074d8858e62a52ea95
Roel Bertens <roelbertens@godatadriven.com>
2023-08-31 21:05:19 +0200

    Align pandas and spark examples and add two spark versions

--- examples/wow/zone_features__pandas.py
+++ examples/wow/zone_features__pandas.py
@@ -3,18 +3,25 @@ import pandas as pd
 from hamilton.function_modifiers import extract_columns
 
 
-@extract_columns("total_count", "darkshore_count")
-def zone_counts(avatarId: pd.Series, zone: pd.Series, aggregation_level: str) -> pd.DataFrame:    
+def with_flags(avatarId: pd.Series, zone: pd.Series) -> pd.DataFrame:
     return (
         pd.concat([avatarId, zone], axis=1)
-        .assign(darkshore_flag=lambda x: (x['zone'] == ' Darkshore').astype(int))            
-        .groupby(aggregation_level)
-        .agg({'avatarId': 'count', 'darkshore_flag': 'sum'})
-        .rename(columns={'avatarId': 'total_count', 'darkshore_flag': 'darkshore_count'})
+        .assign(darkshore_flag=lambda x: (x["zone"] == " Darkshore").astype(int))
+        .assign(durotar_flag=lambda x: (x["zone"] == " Durotar").astype(int))
     )
 
 
-def darkshore_likelyhood(darkshore_count: pd.Series, total_count: pd.Series) -> pd.Series:
-    return (
-        darkshore_count / total_count        
+@extract_columns("total_count", "darkshore_count", "durotar_count")
+def zone_counts(with_flags: pd.DataFrame, aggregation_level: str) -> pd.DataFrame:
+    return with_flags.groupby(aggregation_level).agg(
+        total_count=("darkshore_flag", "count"),
+        darkshore_count=("darkshore_flag", "sum"),
+        durotar_count=("durotar_flag", "sum"),
     )
+
+
+@extract_columns("darkshore_likelihood", "durotar_likelihood")
+def zone_likelihoods(zone_counts: pd.DataFrame) -> pd.DataFrame:
+    return zone_counts.assign(
+        darkshore_likelihood=lambda x: x["darkshore_count"] / x["total_count"]
+    ).assign(durotar_likelihood=lambda x: x["durotar_count"] / x["total_count"])
---
+++ examples/wow/zone_features__spark_v2.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import pyspark.sql as ps
+from pyspark.sql import functions as sf
+
+from hamilton import htypes
+from hamilton.plugins.h_spark import with_columns
+
+
+def spark_session() -> ps.SparkSession:
+    return ps.SparkSession.builder.master("local[1]").getOrCreate()
+
+
+def world_of_warcraft(spark_session: ps.SparkSession) -> ps.DataFrame:
+    return spark_session.read.parquet("data/wow.parquet")
+
+
+def _flag_functions():
+    """Hidden from the DAG by using this wrapper function"""
+    def durotar_flag(zone: pd.Series) -> htypes.column[pd.Series, int]:
+        return (zone == " Durotar").astype(int)
+
+    def darkshore_flag(zone: pd.Series) -> htypes.column[pd.Series, int]:
+        return (zone == " Darkshore").astype(int)
+
+    return durotar_flag, darkshore_flag
+
+
+@with_columns(*_flag_functions(), columns_to_pass=["zone"])
+def with_flags(world_of_warcraft: ps.DataFrame, aggregation_level: str) -> ps.DataFrame:
+    return world_of_warcraft
+
+
+def zone_counts(with_flags: ps.DataFrame, aggregation_level: str) -> ps.DataFrame:
+    return with_flags.groupby(aggregation_level).agg(
+        sf.count("*").alias("total_count"),
+        sf.sum("darkshore_flag").alias("darkshore_count"),
+        sf.sum("durotar_flag").alias("durotar_count"),
+    )
+
+
+def _likelihood_functions():
+    def durotar_likelihood(
+        durotar_count: pd.Series, total_count: pd.Series
+    ) -> htypes.column[pd.Series, float]:
+        return durotar_count / total_count
+
+    def darkshore_likelihood(
+        darkshore_count: pd.Series, total_count: pd.Series
+    ) -> htypes.column[pd.Series, float]:
+        return darkshore_count / total_count
+
+    return durotar_likelihood, darkshore_likelihood
+
+
+@with_columns(
+    *_likelihood_functions(), columns_to_pass=["durotar_count", "darkshore_count", "total_count"]
+)
+def zone_likelihoods(zone_counts: ps.DataFrame) -> ps.DataFrame:
+    return zone_counts
