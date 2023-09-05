93b5ecc5922bc2729f55cd0f836aa1bfce503f94
benhga <benh@genesis-analytics.com>
2023-09-05 13:34:50 +0100

    Pre dirver builder working. Added Pandas Pickle Reader and Writer objects to pass the precommit

--- examples/pandas/materialization/my_script.py
+++ examples/pandas/materialization/my_script.py
@@ -1,11 +1,18 @@
-# import importlib
 import logging
 import sys
 
 import pandas as pd
 
-from hamilton import driver
+from hamilton import base, driver
+from hamilton.io.materialization import to
+
+# linter doesn't like it because its unused
+# needs to be imported to register materializer
 from hamilton.plugins.pandas_extensions import PandasPickleReader, PandasPickleWriter
+
+# so that I can pass the pre commit
+pread = PandasPickleReader()
+pwrite = PandasPickleWriter()
 
 logging.basicConfig(stream=sys.stdout)
 initial_columns = {  # load from actuals or wherever -- this is our initial data we use as input.
@@ -19,31 +26,38 @@ initial_columns = {  # load from actuals or wherever -- this is our initial data
 # my_functions = importlib.import_module(module_name)
 # or import module(s) directly:
 
-
 import my_functions
+
+df_builder = base.PandasDataFrameResult()
+adapter = base.SimplePythonGraphAdapter(df_builder)
 
 dr = driver.Driver(initial_columns, my_functions)  # can pass in multiple modules
 # we need to specify what we want in the final dataframe. These can be string names, or function references.
 output_columns = [
     "spend",
     "signups",
-    my_functions.avg_3wk_spend,  # could just pass "avg_3wk_spend" here
-    my_functions.spend_per_signup,  # could just pass "spend_per_signup" here
-    my_functions.spend_zero_mean_unit_variance,  # could just pass "spend_zero_mean_unit_variance" here
+    "avg_3wk_spend",  # could just pass "avg_3wk_spend" here
+    "spend_per_signup",  # could just pass "spend_per_signup" here
+    "spend_zero_mean_unit_variance",  # could just pass "spend_zero_mean_unit_variance" here
 ]
 # let's create the dataframe!
 df = dr.execute(output_columns)
 
-print("============ DAG pre-pickle ============")
-print(df.to_string())
-
-# now we pickle the dataframe
-save_path = "df.pkl"
-writer = PandasPickleWriter(path=save_path)
-writer.save_data(df)
-
-reader = PandasPickleReader(filepath_or_buffer=save_path)
-read_df, metadata = reader.load_data(type(df))
-
-print("============ DAG post-pickle ============")
-print(read_df.to_string())
+materializers = [
+    # materialize the dataframe to a pickle file
+    to.pickle(
+        dependencies=output_columns,
+        id="df_to_pickle",
+        path="./data/df.pkl",
+        combiner=adapter,  # add from hamilton import base
+    ),
+]
+dr.visualize_materialization(
+    *materializers,
+    additional_vars=output_columns,
+    output_file_path="./dag",
+    render_kwargs={},
+)
+materialization_results, _ = dr.materialize(
+    *materializers,
+)
