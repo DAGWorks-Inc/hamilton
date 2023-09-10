fb7102e52eeffbfcece3efaed4ab8aaad8fc6b15
bryangalindo <galindo.bryan08@gmail.com>
2023-09-10 14:43:20 -0400

    Updates doc strings to use imperative style

--- examples/polars/my_functions.py
+++ examples/polars/my_functions.py
@@ -8,7 +8,7 @@ def base_df(base_df_location: str) -> pl.DataFrame:
     """Loads base dataframe of data.
 
     :param base_df_location: just showing that we could load this from a file...
-    :return:
+    :return: a Polars dataframe
     """
     return pl.DataFrame(
         {
@@ -19,12 +19,12 @@ def base_df(base_df_location: str) -> pl.DataFrame:
 
 
 def avg_3wk_spend(spend: pl.Series) -> pl.Series:
-    """Rolling 3 week average spend."""
+    """Computes rolling 3 week average spend."""
     return spend.rolling_mean(3)
 
 
 def spend_per_signup(spend: pl.Series, signups: pl.Series) -> pl.Series:
-    """The cost per signup in relation to spend."""
+    """Computes cost per signup in relation to spend."""
     return spend / signups
 
 
@@ -39,10 +39,10 @@ def spend_zero_mean(spend: pl.Series, spend_mean: float) -> pl.Series:
 
 
 def spend_std_dev(spend: pl.Series) -> float:
-    """Function that computes the standard deviation of the spend column."""
+    """Computes the standard deviation of the spend column."""
     return spend.std()
 
 
 def spend_zero_mean_unit_variance(spend_zero_mean: pl.Series, spend_std_dev: float) -> pl.Series:
-    """Function showing one way to make spend have zero mean and unit variance."""
+    """Shows one way to make spend have zero mean and unit variance."""
     return spend_zero_mean / spend_std_dev
