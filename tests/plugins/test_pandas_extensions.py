a74abd42d9fa5fb63588de0ba249112a137ed194
Bryan Galindo <galindo.bryan08@gmail.com>
2023-09-18 21:37:19 -0400

    Adds df pytest fixture to adhere to dry

--- tests/plugins/test_pandas_extensions.py
+++ tests/plugins/test_pandas_extensions.py
@@ -17,35 +17,32 @@ from hamilton.plugins.pandas_extensions import (
 )
 
 
-def test_pandas_pickle(tmp_path: pathlib.Path) -> None:
-    data = {
-        "name": ["ladybird", "butterfly", "honeybee"],
-        "num_caught": [4, 5, 6],
-        "stings": [0, 0, 3],
-    }
-    sample_df = pd.DataFrame(data)
+@pytest.fixture
+def df():
+    yield pd.DataFrame({"foo": ["bar"]})
 
-    save_path = tmp_path / "sample_df.pkl"
-    writer = PandasPickleWriter(path=save_path)
-    writer.save_data(sample_df)
 
-    reader = PandasPickleReader(filepath_or_buffer=save_path)
-    read_df, metadata = reader.load_data(type(sample_df))
+def test_pandas_pickle(df: pd.DataFrame, tmp_path: pathlib.Path) -> None:
+    file_path = tmp_path / "sample_df.pkl"
+    writer = PandasPickleWriter(path=file_path)
+    writer.save_data(df)
 
-    # same contents
-    assert read_df.equals(sample_df), "DataFrames do not match"
+    reader = PandasPickleReader(filepath_or_buffer=file_path)
+    read_df, metadata = reader.load_data(pd.DataFrame)
 
-    # correct number of files returned
+    assert read_df.equals(df), "DataFrames do not match"
     assert len(list(tmp_path.iterdir())) == 1, "Unexpected number of files in tmp_path directory."
+    assert file_path.exists()
 
 
-def test_pandas_json(tmp_path: pathlib.Path) -> None:
-    df1 = pd.DataFrame({"foo": ["bar"]})
+def test_pandas_json(df: pd.DataFrame, tmp_path: pathlib.Path) -> None:
     file_path = tmp_path / "test.json"
+
     writer = PandasJsonWriter(filepath_or_buffer=file_path, indent=4)
-    reader = PandasJsonReader(filepath_or_buffer=file_path, encoding="utf-8")
-    writer.save_data(df1)
     kwargs1 = writer._get_saving_kwargs()
+    writer.save_data(df)
+
+    reader = PandasJsonReader(filepath_or_buffer=file_path, encoding="utf-8")
     kwargs2 = reader._get_loading_kwargs()
     df2, metadata = reader.load_data(pd.DataFrame)
 
@@ -54,7 +51,7 @@ def test_pandas_json(tmp_path: pathlib.Path) -> None:
     assert kwargs1["indent"] == 4
     assert kwargs2["encoding"] == "utf-8"
     assert file_path.exists()
-    assert df1.equals(df2)
+    assert df.equals(df2)
 
 
 @pytest.mark.parametrize(
@@ -64,12 +61,12 @@ def test_pandas_json(tmp_path: pathlib.Path) -> None:
         create_engine("sqlite://"),
     ],
 )
-def test_pandas_sql(conn: Union[str, sqlite3.Connection]) -> None:
-    df1 = pd.DataFrame({"foo": ["bar"]})
-    reader = PandasSqlReader(query_or_table="SELECT foo FROM test", db_connection=conn)
+def test_pandas_sql(df: pd.DataFrame, conn: Union[str, sqlite3.Connection]) -> None:
     writer = PandasSqlWriter(table_name="test", db_connection=conn)
-    metadata1 = writer.save_data(df1)
     kwargs1 = writer._get_saving_kwargs()
+    metadata1 = writer.save_data(df)
+
+    reader = PandasSqlReader(query_or_table="SELECT foo FROM test", db_connection=conn)
     kwargs2 = reader._get_loading_kwargs()
     df2, metadata2 = reader.load_data(pd.DataFrame)
 
@@ -77,7 +74,7 @@ def test_pandas_sql(conn: Union[str, sqlite3.Connection]) -> None:
     assert PandasSqlWriter.applicable_types() == [pd.DataFrame]
     assert kwargs1["if_exists"] == "fail"
     assert kwargs2["coerce_float"] is True
-    assert df1.equals(df2)
+    assert df.equals(df2)
 
     if sys.version_info >= (3, 8):
         # py37 pandas 1.3.5 doesn't return rows inserted
