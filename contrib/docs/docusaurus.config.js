e46c21a12530e864550c585e5d230469bbbfa7f2
Stefan Krawczyk <stefan@dagworks.io>
2023-09-28 23:07:37 -0700

    Adds hub social card
    So that it looks like Hamilton.

--- contrib/docs/docusaurus.config.js
+++ contrib/docs/docusaurus.config.js
@@ -74,7 +74,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/hamilton-social-card.png',
       navbar: {
         title: 'Hamilton Dataflow Hub',
         logo: {
