950fb54af8c4960cdc08e0e74e7704923802de85
Kaustubh Ratwadkar <145931905+149189@users.noreply.github.com>
2023-09-29 23:22:16 +0530

    Update executors.py

--- hamilton/execution/executors.py
+++ hamilton/execution/executors.py
@@ -1,14 +1,13 @@
 import abc
 import dataclasses
-import functools
 import logging
-from concurrent.futures import Executor, Future, ProcessPoolExecutor, ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
 from typing import Any, Callable, Dict, List
 
 from hamilton import node
 from hamilton.execution.graph_functions import execute_subdag
 from hamilton.execution.grouping import NodeGroupPurpose, TaskImplementation
-from hamilton.execution.state import ExecutionState, GraphState, TaskState
+from hamilton.execution.state import ExecutionState, TaskState
 
 logger = logging.getLogger(__name__)
 
@@ -16,10 +15,30 @@ logger = logging.getLogger(__name__)
 @dataclasses.dataclass
 class TaskFuture:
     """Simple representation of a future. TODO -- add cancel().
-    This a clean wrapper over a python future, and we may end up just using that at some point."""
+    This is a clean wrapper over a python future, and we may end up just using that at some point."""
 
-    get_state: Callable[[], TaskState]
-    get_result: Callable[[], Any]
+    future: Any
+
+    def get_state(self) -> TaskState:
+        """Gets the state. This is non-blocking."""
+        if self.future.done():
+            try:
+                self.future.result()
+            except Exception:
+                logger.exception("Task failed")
+                return TaskState.FAILED
+            return TaskState.SUCCESSFUL
+        else:
+            return TaskState.RUNNING
+
+    def get_result(self) -> Any:
+        """Gets the result. This is non-blocking.
+
+        :return: None if there is no result, else the result
+        """
+        if not self.future.done():
+            return None
+        return self.future.result()
 
 
 class TaskExecutor(abc.ABC):
@@ -60,54 +79,18 @@ class TaskExecutor(abc.ABC):
         pass
 
 
-def new_callable(*args, _callable=None, **kwargs):
-    return list(_callable(*args, **kwargs))
-
-
-def _modify_callable(node_source: node.NodeType, callabl: Callable):
-    """This is a bit of a shortcut -- we modify the callable here as
-    we want to allow `Parallelizable[]` nodes to return a generaot
-
-    :param node_source:
-    :param callabl:
-    :return:
-    """
-    if node_source == node.NodeType.EXPAND:
-        return functools.partial(new_callable, _callable=callabl)
-    return callabl
-
-
-def base_execute_task(task: TaskImplementation) -> Dict[str, Any]:
-    """This is a utility function to execute a base task. In an ideal world this would be recursive,
-    but for now we just call out to good old DFS.
-
-    We should probably have a simple way of doing this for single-node tasks, as they're
-    going to be common.
-
-    :param task: task to execute.
-    :return: a diciontary of the results of all the nodes in that task's nodes to compute.
-    """
-    # We do this as an edge case to force the callable to return a list if it is an expand,
-    # and would normally return a generator. That said, we will likely remove this in the future --
-    # its an implementation detail, and a true queuing system between nodes/controller would mean
-    # we wouldn't need to do this, and instead could just use the generator aspect.
-    # Furthermore, in most cases the user wouldn't be calling an expand on a "remote" node,
-    # but it is a supported use-case.
-    for node_ in task.nodes:
-        if not getattr(node_, "callable_modified", False):
-            node_._callable = _modify_callable(node_.node_role, node_.callable)
-        setattr(node_, "callable_modified", True)
-    return execute_subdag(
-        nodes=task.nodes,
-        inputs=task.dynamic_inputs,
-        adapter=task.adapters[0],  # TODO -- wire through multiple graph adapters
-        overrides={**task.dynamic_inputs, **task.overrides},
-    )
-
-
 class SynchronousLocalTaskExecutor(TaskExecutor):
     """Basic synchronous/local task executor that runs tasks
     in the same process, at submit time."""
+
+    def __init__(self):
+        self.initialized = False
+
+    def init(self):
+        pass
+
+    def finalize(self):
+        pass
 
     def submit_task(self, task: TaskImplementation) -> TaskFuture:
         """Submitting a task is literally just running it.
@@ -117,7 +100,7 @@ class SynchronousLocalTaskExecutor(TaskExecutor):
         """
         # No error management for now
         result = base_execute_task(task)
-        return TaskFuture(get_state=lambda: TaskState.SUCCESSFUL, get_result=lambda: result)
+        return TaskFuture(future=None, get_state=lambda: TaskState.SUCCESSFUL, get_result=lambda: result)
 
     def can_submit_task(self) -> bool:
         """We can always submit a task as the task submission is blocking!
@@ -126,121 +109,31 @@ class SynchronousLocalTaskExecutor(TaskExecutor):
         """
         return True
 
+
+class ThreadPoolTaskExecutor(TaskExecutor):
+    """Task executor that runs tasks in a ThreadPoolExecutor."""
+
+    def __init__(self, max_workers: int):
+        self.executor = ThreadPoolExecutor(max_workers=max_workers)
+
     def init(self):
         pass
 
     def finalize(self):
-        pass
-
-
-class TaskFutureWrappingPythonFuture(TaskFuture):
-    """Wraps a python future in a TaskFuture"""
-
-    def __init__(self, future: Future):
-        self.future = future
-
-    def get_state(self):
-        """Gets the state. This is non-blocking."""
-
-        if self.future.done():
-            try:
-                self.future.result()
-            except Exception:
-                logger.exception("Task failed")
-                return TaskState.FAILED
-            return TaskState.SUCCESSFUL
-        else:
-            return TaskState.RUNNING
-
-    def get_result(self):
-        """Gets the result. This is non-blocking.
-
-        :return: None if there is no result, else the result
-        """
-        if not self.future.done():
-            return None
-        out = self.future.result()
-        return out
-
-
-class PoolExecutor(TaskExecutor, abc.ABC):
-    """Base class for a pool-based executor (threadpool executor/multiprocessing executor).
-    Handles common logic, stores active future, and manages max tasks.
-    """
-
-    def __init__(self, max_tasks: int):
-        self.active_futures = []
-        self.initialized = False
-        self.pool = None
-        self.max_tasks = max_tasks  # TODO -- allow infinite/no max.
-
-    def _prune_active_futures(self):
-        self.active_futures = [f for f in self.active_futures if not f.done()]
-
-    @abc.abstractmethod
-    def create_pool(self) -> Executor:
-        """Creates a pool to submit tasks to.
-
-        :return: The executor to handle all the tasks in the pool
-        """
-        pass
-
-    def init(self):
-        """Creates/initializes pool"""
-        if not self.initialized:
-            self.pool = self.create_pool()
-            self.initialized = True
-        else:
-            raise RuntimeError("Cannot initialize an already initialized executor")
-
-    def finalize(self):
-        """Finalizes pool, freeing up resources"""
-        if self.initialized:
-            self.pool.shutdown()
-            self.initialized = False
-        else:
-            raise RuntimeError("Cannot finalize an uninitialized executor")
+        self.executor.shutdown()
 
     def submit_task(self, task: TaskImplementation) -> TaskFuture:
-        """Submitting a task is literally just running it.
+        """Submitting a task is running it in a thread from the pool.
 
         :param task: Task to submit
         :return: The future associated with the task
         """
-        # First submit it
-        # Then we need to wrap it in a future
-        future = self.pool.submit(base_execute_task, task)
-        self.active_futures.append(future)
-        return TaskFutureWrappingPythonFuture(future)
+        future = self.executor.submit(base_execute_task, task)
+        return TaskFuture(future=future, get_state=lambda: TaskState.RUNNING, get_result=future.result)
 
     def can_submit_task(self) -> bool:
-        """Tells if the pool is full or not.
-
-        :return:
-        """
-
-        self._prune_active_futures()
-        return len(self.active_futures) < self.max_tasks
-
-
-class MultiThreadingExecutor(PoolExecutor):
-    """Basic synchronous/local task executor that runs tasks
-    in the same process, at submit time."""
-
-    def create_pool(self) -> ThreadPoolExecutor:
-        return ThreadPoolExecutor(max_workers=self.max_tasks)
-
-
-class MultiProcessingExecutor(PoolExecutor):
-    """Basic synchronous/local task executor that runs tasks
-    in the same process, at submit time. Note that this is
-    not yet augmented to handle the right serialization,
-    so use at your own risk. We will be fixing shortly,
-    but the dask/ray parallelism and the multithreading
-    parallelism executors serialize correctly."""
-
-    def create_pool(self) -> ProcessPoolExecutor:
-        return ProcessPoolExecutor(max_workers=self.max_tasks)
+        # For ThreadPoolExecutor, we can always submit a task
+        return True
 
 
 class ExecutionManager(abc.ABC):
@@ -248,24 +141,6 @@ class ExecutionManager(abc.ABC):
     tasks/task types. Note that, currently, it just uses the task information, but we could
     theoretically add metadata in a task as well.
     """
-
-    def __init__(self, executors: List[TaskExecutor]):
-        """Initializes the execution manager. Note this does not start it up/claim resources --
-        you need to call init() to do that.
-
-        :param executors:
-        """
-        self.executors = executors
-
-    def init(self):
-        """Initializes each of the executors."""
-        for executor in self.executors:
-            executor.init()
-
-    def finalize(self):
-        """Finalizes each of the executors."""
-        for executor in self.executors:
-            executor.finalize()
 
     @abc.abstractmethod
     def get_executor_for_task(self, task: TaskImplementation) -> TaskExecutor:
@@ -282,29 +157,24 @@ class ExecutionManager(abc.ABC):
 
 
 class DefaultExecutionManager(ExecutionManager):
-    def __init__(self, local_executor=None, remote_executor=None):
-        """Instantiates a BasicExecutionManager with a local/remote executor.
+    def __init__(self, local_executor: TaskExecutor, remote_executor: TaskExecutor):
+        """Instantiates an ExecutionManager with a local/remote executor.
         These enable us to run certain tasks locally (simple transformations, generating sets of files),
         and certain tasks remotely (processing files in large datasets, etc...)
 
         :param local_executor: Executor to use for running tasks locally
         :param remote_executor:  Executor to use for running tasks remotely
         """
-        if local_executor is None:
-            local_executor = SynchronousLocalTaskExecutor()
-        if remote_executor is None:
-            remote_executor = MultiProcessingExecutor(max_tasks=5)
-        super().__init__([local_executor, remote_executor])
         self.local_executor = local_executor
         self.remote_executor = remote_executor
 
     def get_executor_for_task(self, task: TaskImplementation) -> TaskExecutor:
         """Simple implementation that returns the local executor for single task executions,
+        and the remote executor for tasks to execute in a separate process.
 
         :param task: Task to get executor for
-        :return: A local task if this is a "single-node" task, a remote task otherwise
+        :return: A local task executor if this is a "single-node" task, a remote task executor otherwise
         """
-
         if task.purpose == NodeGroupPurpose.EXECUTE_BLOCK:
             return self.remote_executor
         return self.local_executor
@@ -314,16 +184,11 @@ def run_graph_to_completion(
     execution_state: ExecutionState,
     execution_manager: ExecutionManager,
 ):
-    """Blocking call to run the graph until it is complete. Note that this employs a while loop.
-    With the way we handle futures, we should be able to have this event-driven, allowing us
-    to only query when the state is updated, and use that to trigger a new update.
-
-    For now, the while loop is fine.
+    """Blocking call to run the graph until it is complete. This employs a while loop.
 
     :return: Nothing, the execution state/result cache can give us the data
     """
     task_futures = {}
-    execution_manager.init()
     try:
         while not GraphState.is_terminal(execution_state.get_graph_state()):
             # get the next task from the queue
@@ -355,4 +220,66 @@ def run_graph_to_completion(
                     del task_futures[task_name]
         logger.info(f"Graph is done, graph state is {execution_state.get_graph_state()}")
     finally:
-        execution_manager.finalize()
+        # Ensure proper cleanup
+        for task_name, task_future in task_futures.items():
+            if not task_future.future.done():
+                # Cancel any remaining tasks
+                task_future.future.cancel()
+
+
+# Utility function to execute a base task
+def base_execute_task(task: TaskImplementation) -> Dict[str, Any]:
+    """This is a utility function to execute a base task.
+
+    :param task: task to execute.
+    :return: a dictionary of the results of all the nodes in that task's nodes to compute.
+    """
+    for node_ in task.nodes:
+        if not getattr(node_, "callable_modified", False):
+            node_._callable = _modify_callable(node_.node_role, node_.callable)
+        setattr(node_, "callable_modified", True)
+    return execute_subdag(
+        nodes=task.nodes,
+        inputs=task.dynamic_inputs,
+        adapter=task.adapters[0],  # TODO -- wire through multiple graph adapters
+        overrides={**task.dynamic_inputs, **task.overrides},
+    )
+
+
+def new_callable(*args, _callable=None, **kwargs):
+    return list(_callable(*args, **kwargs))
+
+
+def _modify_callable(node_source: node.NodeType, callabl: Callable):
+    """This is a bit of a shortcut -- we modify the callable here as
+    we want to allow `Parallelizable[]` nodes to return a generator
+
+    :param node_source:
+    :param callabl:
+    :return:
+    """
+    if node_source == node.NodeType.EXPAND:
+        return functools.partial(new_callable, _callable=callabl)
+    return callabl
+
+
+# Example usage
+if __name__ == "__main__":
+    local_executor = SynchronousLocalTaskExecutor()
+    thread_pool_executor = ThreadPoolTaskExecutor(max_workers=5)
+
+    execution_manager = DefaultExecutionManager(local_executor=local_executor, remote_executor=thread_pool_executor)
+
+    # Create a dummy task to demonstrate task execution
+    dummy_task = TaskImplementation(
+        task_id="dummy_task",
+        nodes=[],  # Add your nodes here
+        dynamic_inputs={},  # Add your dynamic inputs here
+        adapters=[],  # Add your adapters here
+        purpose=NodeGroupPurpose.EXECUTE_BLOCK,  # Adjust the purpose as needed
+    )
+
+    execution_state = ExecutionState()  # Assuming you have an ExecutionState instance
+
+    run_graph_to_completion(execution_state, execution_manager)  # Run the task graph to completion
+
