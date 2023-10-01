26734d960008f8bcae7e97b41c740c0fd106cb5d
Kaustubh Ratwadkar <145931905+149189@users.noreply.github.com>
2023-10-01 21:15:00 +0530

    Delete pandas table directory

--- pandas table/pandas_extensions.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-class CustomPandasLoader:
-    def __init__(self, filepath, **kwargs):
-        self.filepath = filepath
-        self.kwargs = kwargs
-
-    def load(self):
-        return pd.read_table(self.filepath, **self.kwargs)
