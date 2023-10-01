4d4bcbe41630b6ca5727f1839be71ba64db03000
Kaustubh Ratwadkar <145931905+149189@users.noreply.github.com>
2023-10-01 23:04:57 +0530

    Update pandas_extensions.py

--- hamilton/plugins/pandas_extensions.py
+++ hamilton/plugins/pandas_extensions.py
@@ -8,6 +8,7 @@ from pathlib import Path
 from typing import Any, Callable, Collection, Dict, Iterator, List, Optional, Tuple, Type, Union
 import pandas as pd
 from io import BytesIO, BufferedReader
+from typing import Union, Dict, Any, Type, Tuple, Collection, Optional
 from pandas import DataFrame
 from pandas.api.types import infer_dtype
 from pandas.io.common import get_handle
@@ -1199,6 +1200,7 @@ class PandasFeatherWriter(DataSaver):
 
 
 def register_data_loaders():
+    """Function to register the data loaders for this extension."""
     for loader in [
         CSVDataAdapter,
         ParquetDataLoader,
