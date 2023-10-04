ff6d5ef210299a6e316f99883e6004b36550b853
Stefan Krawczyk <stefan@dagworks.io>
2023-10-04 13:23:14 -0700

    Refactors converting output values into strings
    So both the driver and materializer need to handle converting
    function names and variables into strings.
    
    This is a little messy -- but it centralizes logic in common.
    I didn't bother with another file because I didn't know what to call it.
    So putting under common seems fine.
    
    Otherwise I added tests to ensure that new functionality works,
    and left the existing tests to ensure nothing broke.

---
+++ tests/test_common.py
@@ -0,0 +1,60 @@
+import pytest
+
+import tests.resources.cyclic_functions
+import tests.resources.test_default_args
+from hamilton import common, driver
+
+
+class Object:
+    """Dummy class to test with."""
+
+    def __repr__(self):
+        return "'object'"
+
+
+@pytest.mark.parametrize(
+    "value_to_convert, module_set, expected_value, expected_error",
+    [
+        ("a", {"amodule"}, "a", None),
+        (
+            tests.resources.test_default_args.A,
+            {tests.resources.test_default_args.__name__},
+            "A",
+            None,
+        ),
+        (driver.Variable("A", int), {"amodule"}, "A", None),
+        (
+            Object(),
+            {"amodule"},
+            None,
+            "Materializer dependency 'object' is not a string, a function, or a driver.Variable.",
+        ),
+        (
+            tests.resources.cyclic_functions.A,
+            {tests.resources.test_default_args.__name__},
+            None,
+            "Function tests.resources.cyclic_functions.A is a function not in a module given to the materializer. Valid choices are {'tests.resources.test_default_args'}.",
+        ),
+    ],
+)
+def test_convert_output_value(value_to_convert, module_set, expected_value, expected_error):
+    actual_value, actual_error = common.convert_output_value(value_to_convert, module_set)
+    assert actual_value == expected_value
+    assert actual_error == expected_error
+
+
+def test_convert_output_values_happy():
+    """Tests that we loop as expected without issue"""
+    actual = common.convert_output_values(
+        [tests.resources.test_default_args.A, "B"], {tests.resources.test_default_args.__name__}
+    )
+    assert actual == ["A", "B"]
+
+
+def test_convert_output_values_error():
+    """Tests that we error when bad cases are encountered."""
+    with pytest.raises(ValueError):
+        common.convert_output_values(
+            [tests.resources.test_default_args.A, tests.resources.cyclic_functions.A],
+            {tests.resources.test_default_args.__name__},
+        )
