2da6f5d67fc70a7b07331e13c6e89dd937936e01
Vivek Praharsha <vpraharsha@outlook.com>
2023-10-18 12:53:37 +0530

    Fix review points

--- hamilton/plugins/sklearn_plot_extensions.py
+++ hamilton/plugins/sklearn_plot_extensions.py
@@ -1,15 +1,9 @@
 import dataclasses
 from os import PathLike
-from typing import Any, Collection, Dict, Tuple, Type, Union
-
+from typing import Any, Collection, Dict, Type, Union
 
 try:
     import sklearn.metrics
-except ImportError:
-    raise NotImplementedError("scikit-learn is not installed.")
-
-try:
-    from sklearn.exceptions import NotFittedError
 except ImportError:
     raise NotImplementedError("scikit-learn is not installed.")
 
@@ -22,12 +16,24 @@ from hamilton import registry
 from hamilton.io import utils
 from hamilton.io.data_adapters import DataSaver
 
+SKLEARN_PLOT_TYPES = [
+    sklearn.metrics.ConfusionMatrixDisplay,
+    sklearn.metrics.DetCurveDisplay,
+    sklearn.metrics.PrecisionRecallDisplay,
+    sklearn.metrics.PredictionErrorDisplay,
+    sklearn.metrics.RocCurveDisplay,
+]
+SKLEARN_PLOT_TYPES_ANNOTATION = Union[
+    sklearn.metrics.ConfusionMatrixDisplay,
+    sklearn.metrics.DetCurveDisplay,
+    sklearn.metrics.PrecisionRecallDisplay,
+    sklearn.metrics.PredictionErrorDisplay,
+    sklearn.metrics.RocCurveDisplay,
+]
 
-SKLEARN_PLOT_TYPES = [sklearn.metrics.ConfusionMatrixDisplay, sklearn.metrics.DetCurveDisplay, sklearn.metrics.PrecisionRecallDisplay, sklearn.metrics.PredictionErrorDisplay, sklearn.metrics.RocCurveDisplay]
-SKLEARN_PLOT_TYPES_ANNOTATION = Union[sklearn.metrics.ConfusionMatrixDisplay, sklearn.metrics.DetCurveDisplay, sklearn.metrics.PrecisionRecallDisplay, sklearn.metrics.PredictionErrorDisplay, sklearn.metrics.RocCurveDisplay]
 
 @dataclasses.dataclass
-class ConfusionMatrixPlotSaver(DataSaver):
+class SklearnPlotSaver(DataSaver):
 
     path: Union[str, PathLike]
 
@@ -35,19 +41,18 @@ class ConfusionMatrixPlotSaver(DataSaver):
     def applicable_types(cls) -> Collection[Type]:
         return SKLEARN_PLOT_TYPES
 
-    def save_plot(self, data: SKLEARN_PLOT_TYPES_ANNOTATION):
-        try:
-            plt.savefig(self.path, dpi=200)
-        except NotFittedError as e:
-            return utils.get_exception_details(e)
+    def save_plot(self, data: SKLEARN_PLOT_TYPES_ANNOTATION) -> Dict[str, Any]:
+        plt.savefig(self.path, dpi=200)
         return utils.get_file_metadata(self.path)
-    
+
     @classmethod
     def name(cls) -> str:
-        return ""  # TODO: Decide on name
-    
+        return "png"
+
+
 def register_data_loaders():
     """Function to register the data loaders for this extension."""
-    registry.register_adapter(ConfusionMatrixPlotSaver)
+    registry.register_adapter(SklearnPlotSaver)
+
 
 register_data_loaders()
