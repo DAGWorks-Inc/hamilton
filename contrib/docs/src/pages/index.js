148597f6b8dc5553483bc83a79698df38262c4f6
Stefan Krawczyk <stefan@dagworks.io>
2023-11-20 22:03:45 -0800

    Fixes hub page header
    Fixes the header value, and description.
    
    Also adds to some wording on the landing page.

--- contrib/docs/src/pages/index.js
+++ contrib/docs/src/pages/index.js
@@ -30,8 +30,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Off-the-shelf Hamilton Dataflows ready for you to use.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
