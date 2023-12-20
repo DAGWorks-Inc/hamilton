332e71f68328714a5b652441cf86b4f241f806e0
Stefan Krawczyk <stefan@dagworks.io>
2023-12-19 16:30:40 -0800

    Exposes ray init functionality
    This was bothering me that we hadn't exposed a few things, this fixes that.
    It means that someone can now connect to a ray cluster for example.

--- hamilton/plugins/h_ray.py
+++ hamilton/plugins/h_ray.py
@@ -209,20 +209,33 @@ class RayTaskExecutor(executors.TaskExecutor):
     This is still experimental, so the API might change.
     """
 
-    def __init__(self, num_cpus: int):
+    def __init__(
+        self,
+        num_cpus: int = None,
+        config: typing.Dict[str, typing.Any] = None,
+        skip_init: bool = False,
+    ):
         """Creates a ray task executor. Note this will likely take in more parameters. This is
         experimental, so the API will likely change, although we will do our best to make it
         backwards compatible.
 
-        :param num_cpus: Number of cores to use for initialization, passed drirectly to ray.init
+        :param num_cpus: Number of cores to use for initialization, passed directly to ray.init. Defaults to all cores.
+        :param config: General configuration to pass to ray.init. Defaults to None.
+        :param skip_init: Skips ray init if you already have Ray initialized. Default is False.
         """
         self.num_cpus = num_cpus
+        self.config = config if config else {}
+        self.skip_init = skip_init
 
     def init(self):
-        ray.init(num_cpus=self.num_cpus)
+        if not self.skip_init:
+            ray.init(num_cpus=self.num_cpus, **self.config)
 
     def finalize(self):
-        ray.shutdown()
+        if (
+            not self.skip_init
+        ):  # we assume that if we didn't init it, we don't need to shutdown either.
+            ray.shutdown()
 
     def submit_task(self, task: TaskImplementation) -> TaskFuture:
         """Submits a task, wrapping it in a TaskFuture (after getting the corresponding python
