8b65c77da1f21da0d6474022c0bef2dd62207de0
elijahbenizzy <elijah_ben_izzy@alumni.brown.edu>
2024-01-14 15:08:20 -0800

    Updates TQDM hook to use newer API + have better options
    This does the following:
    
    1. Uses the new external-facing API to demonstrate how it should be used
    2. Fixes up padding so we automatically derive it to be the maximum (and
       thus ensure it won't be jumpy). We also have a max so we don't exceed
       the line, this defaults to 50.

--- hamilton/lifecycle/conditional_tqdm.py
+++ hamilton/lifecycle/conditional_tqdm.py
@@ -2,15 +2,13 @@ from typing import Any, Dict, List, Optional
 
 import tqdm
 
-from hamilton import graph, node
-from hamilton.lifecycle import base
+from hamilton import graph_types
+from hamilton.lifecycle import GraphExecutionHook, NodeExecutionHook
 
 
 class TQDM(
-    base.BasePreGraphExecute,
-    base.BasePreNodeExecute,
-    base.BasePostNodeExecute,
-    base.BasePostGraphExecute,
+    GraphExecutionHook,
+    NodeExecutionHook,
 ):
     """An adapter that uses tqdm to show progress bars for the graph execution.
 
@@ -33,71 +31,70 @@ class TQDM(
 
     """
 
-    def __init__(self, desc: str = "Graph execution", node_name_target_width: int = 30, **kwargs):
+    def __init__(self, desc: str = "Graph execution", max_node_name_width: int = 50, **kwargs):
         """Create a new TQDM Adatper.
 
         :param desc: The description to show in the progress bar. E.g. DAG Name is a good choice.
         :param kwargs: Additional kwargs to pass to TQDM. See TQDM docs for more info.
-        :param node_name_target_width: the target width for the node name so that the progress bar is consistent.
+        :param node_name_target_width: the target width for the node name so that the progress bar is consistent. If this is None, it will take the longest, until it hits max_node_name_width.
+
         """
         self.desc = desc
         self.kwargs = kwargs
-        self.node_name_target_width = node_name_target_width  # what we target padding for.
+        self.node_name_target_width = (
+            None  # what we target padding for -- starts at None as we adjust.
+        )
+        self.max_node_name_width = max_node_name_width  # what we cap the padding at.
+        self.progress_bar = None
 
-    def pre_graph_execute(
+    def _get_node_name_display(self, node_name: str) -> str:
+        """Gives the node name display given a max width and a node name. Max width could be DAG-dependent."""
+        out = (
+            node_name
+            if len(node_name) <= self.node_name_target_width
+            else node_name[: self.node_name_target_width - 3] + "..."
+        )
+        if len(out) < self.node_name_target_width:
+            out += " " * (self.node_name_target_width - len(out))
+        return out
+
+    def run_before_graph_execution(
         self,
         *,
-        run_id: str,
-        graph: graph.FunctionGraph,
+        graph: graph_types.HamiltonGraph,
         final_vars: List[str],
         inputs: Dict[str, Any],
         overrides: Dict[str, Any],
+        **future_kwargs: Any,
     ):
-        all, human = graph.get_upstream_nodes(final_vars)
-        total_node_to_execute = len(all) - len(human)
+        nodes_to_execute = graph.get_execution_nodes(inputs, final_vars, overrides)
+        total_node_to_execute = len(nodes_to_execute)
+        max_node_name_length = min(
+            max([len(node.name) for node in nodes_to_execute]), self.max_node_name_width
+        )
+        if self.node_name_target_width is None:
+            self.node_name_target_width = max_node_name_length
         self.progress_bar = tqdm.tqdm(
             desc=self.desc, unit="funcs", total=total_node_to_execute, **self.kwargs
         )
 
-    def pre_node_execute(
+    def run_before_node_execution(
         self,
         *,
-        run_id: str,
-        node_: node.Node,
-        kwargs: Dict[str, Any],
-        task_id: Optional[str] = None,
+        node_name: str,
+        node_tags: Dict[str, Any],
+        node_kwargs: Dict[str, Any],
+        node_return_type: type,
+        task_id: Optional[str],
+        **future_kwargs: Any,
     ):
-        # pad the node name with spaces to make it consistent in length
-        name_part = node_.name
-        if len(name_part) > self.node_name_target_width:
-            padding = ""
-        else:
-            padding = " " * (self.node_name_target_width - len(name_part))
-        self.progress_bar.set_description_str(f"{self.desc} -> {name_part + padding}")
-        self.progress_bar.set_postfix({"executing": name_part})
+        name_display = self._get_node_name_display(node_name)
+        self.progress_bar.set_description_str(f"{self.desc} -> {name_display}")
 
-    def post_node_execute(
-        self,
-        *,
-        run_id: str,
-        node_: node.Node,
-        kwargs: Dict[str, Any],
-        success: bool,
-        error: Optional[Exception],
-        result: Optional[Any],
-        task_id: Optional[str] = None,
-    ):
+    def run_after_node_execution(self, **future_kwargs):
         self.progress_bar.update(1)
 
-    def post_graph_execute(
-        self,
-        *,
-        run_id: str,
-        graph: graph.FunctionGraph,
-        success: bool,
-        error: Optional[Exception],
-        results: Optional[Dict[str, Any]],
-    ):
+    def run_after_graph_execution(self, **future_kwargs):
         name_part = "Execution Complete!"
         if len(name_part) > self.node_name_target_width:
             padding = ""
