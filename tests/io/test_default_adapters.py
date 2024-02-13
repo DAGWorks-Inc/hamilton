aa1d250c2cf958efe39b3dee4521a984ffea9e1d
zilto <tjean@DESKTOP-V6JDCS2>
2024-02-13 14:02:01 -0500

    Tests for JSON types Dict and List[dict] added
    The Python json library allows to save tuples and
    other collections as JSON. However, they would loaded
    as a list. Hence, we limit the type to dict and list[dict]

--- tests/io/test_default_adapters.py
+++ tests/io/test_default_adapters.py
@@ -1,9 +1,16 @@
 import io
+import json
 import pathlib
+from typing import List
 
 import pytest
 
-from hamilton.io.default_data_loaders import RawFileDataSaverBytes
+from hamilton.io.utils import FILE_METADATA
+from hamilton.io.default_data_loaders import (
+    RawFileDataSaverBytes,
+    JSONDataLoader,
+    JSONDataSaver
+)
 
 
 @pytest.mark.parametrize(
@@ -24,3 +31,39 @@ def test_raw_file_adapter(data, tmp_path: pathlib.Path) -> None:
 
     data_processed = data if type(data) is bytes else data.getvalue()
     assert data_processed == data2
+
+
+@pytest.mark.parametrize(
+    "data", [{"key": "value"}, [{"key": "value1"}, {"key": "value2"}]]
+)
+def test_json_save_object_and_array(data, tmp_path: pathlib.Path):
+    """Test that `from_.json` and `to.json` can handle JSON objects where
+    the top-level is an object `{ }` -> dict or an array `[ ]` -> list[dict]
+    """
+    data_path = tmp_path / "data.json"
+    saver = JSONDataSaver(path=data_path)
+    
+    metadata = saver.save_data(data)
+    loaded_data = json.loads(data_path.read_text())
+        
+    assert JSONDataSaver.applicable_types() == [dict, List[dict]]
+    assert data_path.exists()
+    assert metadata[FILE_METADATA]["path"] == str(data_path)
+    assert data == loaded_data
+    
+    
+@pytest.mark.parametrize(
+    "data", [{"key": "value"}, [{"key": "value1"}, {"key": "value2"}]]
+)
+def test_json_load_object_and_array(data, tmp_path: pathlib.Path):
+    """Test that `from_.json` and `to.json` can handle JSON objects where
+    the top-level is an object `{ }` -> dict or an array `[ ]` -> list[dict]
+    """
+    data_path = tmp_path / "data.json"
+    loader = JSONDataLoader(path=data_path)
+    
+    json.dump(data, data_path.open("w"))
+    loaded_data, metadata = loader.load_data(type(data))
+        
+    assert JSONDataLoader.applicable_types() == [dict, List[dict]]
+    assert data == loaded_data
