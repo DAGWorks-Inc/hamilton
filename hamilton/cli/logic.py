409e996f18fb4f69e8c091e52cf003ef26737ed8
zilto <tjean@DESKTOP-V6JDCS2>
2024-03-04 11:45:25 -0500

    changed error type to make bot happy

--- hamilton/cli/logic.py
+++ hamilton/cli/logic.py
@@ -26,7 +26,7 @@ def get_git_base_directory() -> str:
             return result.stdout.strip()
         else:
             print("Error:", result.stderr.strip())
-            raise ValueError(f"{result.stderr.strip()}")
+            raise OSError(f"{result.stderr.strip()}")
     except FileNotFoundError:
         raise FileNotFoundError("Git command not found. Please make sure Git is installed.")
 
