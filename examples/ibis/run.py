6fdeed86a0b250eb41af4d3a2a1196b4f8bbc177
zilto <tjean@DESKTOP-V6JDCS2>
2024-03-05 14:11:36 -0500

    split ibis and ibisml

--- examples/ibis/run.py
+++ examples/ibis/run.py
@@ -1,3 +1,7 @@
+from hamilton import driver
+from hamilton.plugins.h_tqdm import ProgressBar
+
+
 def view_expression(expression, **kwargs):
     """View an Ibis expression
 
@@ -11,39 +15,20 @@ def view_expression(expression, **kwargs):
     return dot
 
 
-def main(level: str, model: str):
-    dataflow_components = []
-    config = {}
-    final_vars = ["feature_set"]
-
+def main(level: str):
     if level == "column":
         import column_dataflow
 
-        dataflow_components.append(column_dataflow)
+        feature_dataflow = column_dataflow
     elif level == "table":
         import table_dataflow
 
-        dataflow_components.append(table_dataflow)
+        feature_dataflow = table_dataflow
     else:
         raise ValueError("`level` must be in ['column', 'table']")
 
-    if model:
-        import model_training
-
-        dataflow_components.append(model_training)
-        config["model"] = model
-        final_vars.extend(["full_model", "fitted_recipe", "cross_validation_scores"])
-
     # build the Driver from modules
-    dr = (
-        driver.Builder()
-        .with_modules(*dataflow_components)
-        .with_config(config)
-        .with_adapters(ProgressBar())
-        .enable_dynamic_execution(allow_experimental_mode=True)
-        .with_remote_executor(SynchronousLocalTaskExecutor())
-        .build()
-    )
+    dr = driver.Builder().with_modules(feature_dataflow).with_adapters(ProgressBar()).build()
 
     inputs = dict(
         raw_data_path="../data_quality/simple/Absenteeism_at_work.csv",
@@ -56,28 +41,20 @@ def main(level: str, model: str):
             "disciplinary_failure",
             "absenteeism_time_in_hours",
         ],
-        label="absenteeism_time_in_hours",
-    )
-    dr.visualize_execution(
-        final_vars=final_vars, inputs=inputs, output_file_path="cross_validation.png"
     )
 
-    res = dr.execute(final_vars, inputs=inputs)
+    res = dr.execute(["feature_set"], inputs=inputs)
     view_expression(res["feature_set"], filename="ibis_feature_set", format="png")
 
-    print(res.keys())
+    print("Dataflow result keys: ", list(res.keys()))
 
 
 if __name__ == "__main__":
     import argparse
 
-    from hamilton import driver
-    from hamilton.execution.executors import SynchronousLocalTaskExecutor
-    from hamilton.plugins.h_tqdm import ProgressBar
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("--level", choices=["column", "table"])
-    parser.add_argument("--model", choices=["linear", "random_forest", "boosting"])
+    parser.add_argument("--level", choices=["column", "table"], default="table")
     args = parser.parse_args()
 
-    main(level=args.level, model=args.model)
+    print(f"Running dataflow at {args.level} level")
+    main(level=args.level)
