8689202823a5a7932587e0418cb85d8c38da9ded
Stefan Krawczyk <stefan@dagworks.io>
2024-03-22 11:17:53 -0700

    Fixes skipif logic (#778)
    * Fixes skipif logic
    
    Argh, didn't properly evaluate the condition.
    This should work now.
    
    * Fixes isort issue by skipping it
    
    Pre-commit run --all-files still has different behavior
    than the pre-commit hook for me. This just tells isort
    to skip things. I'm not sure where the discrepancy comes from.

--- tests/plugins/test_polars_extensions.py
+++ tests/plugins/test_polars_extensions.py
@@ -1,10 +1,10 @@
 import pathlib
 import sys
 
-import pytest
+import polars as pl  # isort: skip
+import pytest  # isort: skip
 
-import polars as pl
-from hamilton.plugins.polars_extensions import (
+from hamilton.plugins.polars_extensions import (  # isort: skip
     PolarsAvroReader,
     PolarsAvroWriter,
     PolarsCSVReader,
@@ -120,7 +120,10 @@ def test_polars_avro(df: pl.DataFrame, tmp_path: pathlib.Path) -> None:
     assert df.frame_equal(df2)
 
 
-@pytest.mark.skipif(sys.version_info == (3, 12), reason="weird connectorx error on 3.12")
+@pytest.mark.skipif(
+    sys.version_info.major == 3 and sys.version_info.minor == 12,
+    reason="weird connectorx error on 3.12",
+)
 def test_polars_database(df: pl.DataFrame, tmp_path: pathlib.Path) -> None:
     conn = f"sqlite:///{tmp_path}/test.db"
     table_name = "test_table"
