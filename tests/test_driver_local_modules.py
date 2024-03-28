1c4b19bd875c33fe75903fc94027cfc09186833f
swapdewalkar@gmail.com <swapdewalkar@gmail.com>
2024-03-28 21:24:37 +0530

    Added test case

---
+++ tests/test_driver_local_modules.py
@@ -0,0 +1,11 @@
+from hamilton.driver import Builder, DefaultGraphExecutor
+
+
+def test_driver_with_local_modules() -> None:
+    dr = (
+        Builder()
+        .with_local_modules()
+        .build()
+    )
+    assert isinstance(dr.graph_executor, DefaultGraphExecutor)
+    assert __name__ == list(dr.graph_modules)[0].__name__
