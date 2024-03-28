8945bad5ba29603c3e520c03cac8bf30a5a26bcd
swapdewalkar@gmail.com <swapdewalkar@gmail.com>
2024-03-28 20:48:10 +0530

    Initial Working

--- hamilton/driver.py
+++ hamilton/driver.py
@@ -1669,6 +1669,18 @@ class Builder:
         self.config.update(config)
         return self
 
+    def with_local_modules(self) -> "Builder":
+        """Adds the specified modules to the modules list.
+        This can be called multiple times -- later calls will take precedence.
+
+        :param file_path: Path to the file to import.
+        :return: self
+        """
+        import inspect
+        module = inspect.getmodule(inspect.stack()[1][0])
+        self.modules.append(module)
+        return self
+
     def with_modules(self, *modules: ModuleType) -> "Builder":
         """Adds the specified modules to the modules list.
         This can be called multiple times -- later calls will take precedence.
