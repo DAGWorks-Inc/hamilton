8f89add221cc59096bba185d706d86b07f79d769
Stefan Krawczyk <stefan@dagworks.io>
2024-04-10 22:05:45 -0700

    Adds NER example using HF datasets & lancedb
    This shows how one might build a pipeline and utilize
    models to extract entities and embeddings. Then
    save them to lancedb, and then use both to query
    over them.
    
    WIP (+4 squashed commits)
    Squashed commits:
    [c91afb69] wip
    [17cd2976] Gets example to run on HF datasets properly
    
    TODOs:
     - tidy up
     - README
     - remove parallel in favor of discussion
    [f8409340] TODOs:
    
    1. remove parallel - doesn't make sense for GPU case as you can't parallelize that, and you want to use datasets.map() for batching.
    2. make it run on datasets
    [b76d0110] WIP create lanceDB NER example

---
+++ examples/LLM_Workflows/NER_Example/run.py
@@ -0,0 +1,89 @@
+import argparse
+
+import lancedb_module
+import ner_extraction
+
+from hamilton import driver, lifecycle
+
+
+def build_driver(adapter_list):
+    """Builds the driver with the necessary modules and adapters."""
+    dr = (
+        driver.Builder()
+        .with_config({})
+        .with_modules(ner_extraction, lancedb_module)
+        .with_adapters(*adapter_list)
+        .build()
+    )
+    return dr
+
+
+def load_data(table_name: str, use_tracker: bool = False):
+    adapter_list = [lifecycle.PrintLn()]
+    if use_tracker:
+        from hamilton_sdk import adapters
+
+        tracker = adapters.HamiltonTracker(
+            project_id=41,  # modify this as needed
+            username="elijah@dagworks.io",
+            dag_name="ner-lancedb-pipeline",
+            tags={"context": "extraction", "team": "MY_TEAM", "version": "1"},
+        )
+        adapter_list.append(tracker)
+
+    dr = build_driver(adapter_list)
+    # display the graph
+    dr.display_all_functions("ner_extraction_pipeline.png")
+
+    results = dr.execute(
+        ["load_into_lancedb"],
+        inputs={"table_name": table_name},
+    )
+    print(results)
+
+
+def query_data(query: str, table_name: str, use_tracker: bool = False):
+    adapter_list = [lifecycle.PrintLn()]
+    if use_tracker:
+        from hamilton_sdk import adapters
+
+        tracker = adapters.HamiltonTracker(
+            project_id=41,  # modify this as needed
+            username="elijah@dagworks.io",
+            dag_name="ner-lancedb-pipeline",
+            tags={"context": "inference", "team": "MY_TEAM", "version": "1"},
+        )
+        adapter_list.append(tracker)
+
+    dr = build_driver(adapter_list)
+
+    r = dr.execute(["lancedb_result"], inputs={"query": query, "table_name": table_name})
+    print(r)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Process command-line arguments.")
+    parser.add_argument("table_name", help="The name of the table.")
+    parser.add_argument("operation", choices=["load", "query"], help="The operation to perform.")
+    parser.add_argument("--query", help="The query to run. Required if operation is 'query'.")
+    parser.add_argument("--use-tracker", action="store_true", help="Whether to use the tracker.")
+
+    args = parser.parse_args()
+
+    if args.operation == "query" and args.query is None:
+        parser.error("The --query argument is required when operation is 'query'.")
+
+    if args.operation == "load":
+        load_data(args.table_name, args.use_tracker)
+    else:
+        query_data(args.query, args.table_name, args.use_tracker)
+
+
+if __name__ == "__main__":
+    """
+    Some example commands:
+    > python run.py medium_docs load
+    > python run.py medium_docs query --query "Why does SpaceX want to build a city on Mars?"
+    > python run.py medium_docs query --query "How are autonomous vehicles changing the world?"
+    """
+    main()
