29224e88e743d2fc4dae25d06fb289c0a91a4122
wmoreiraa <walber3@gmail.com>
2024-04-12 16:38:44 -0300

    feature: added yaml loaders as plugin instead of default adapter
    Signed-off-by: wmoreiraa <walber3@gmail.com>

--- hamilton/io/default_data_loaders.py
+++ hamilton/io/default_data_loaders.py
@@ -6,54 +6,8 @@ import pathlib
 import pickle
 from typing import Any, Collection, Dict, Tuple, Type, Union
 
-import yaml
-
 from hamilton.io.data_adapters import DataLoader, DataSaver
 from hamilton.io.utils import get_file_metadata
-
-PrimitiveType = Union[str, int, bool, dict, list]
-
-
-@dataclasses.dataclass
-class YAMLDataLoader(DataLoader):
-    path: Union[str, pathlib.Path]
-
-    @classmethod
-    def applicable_types(cls) -> Collection[Type]:
-        return [str, int, bool, dict, list]
-
-    @classmethod
-    def name(cls) -> str:
-        return "yaml"
-
-    def load_data(self, type_: Type) -> Tuple[PrimitiveType, Dict[str, Any]]:
-        path = self.path
-        if isinstance(self.path, str):
-            path = pathlib.Path(self.path)
-
-        with path.open(mode="r") as f:
-            return yaml.safe_load(f), get_file_metadata(path)
-
-
-@dataclasses.dataclass
-class YAMLDataSaver(DataSaver):
-    path: Union[str, pathlib.Path]
-
-    @classmethod
-    def applicable_types(cls) -> Collection[Type]:
-        return [str, int, bool, dict, list]
-
-    @classmethod
-    def name(cls) -> str:
-        return "yaml"
-
-    def save_data(self, data: Any) -> Dict[str, Any]:
-        path = self.path
-        if isinstance(path, str):
-            path = pathlib.Path(path)
-        with path.open("w") as f:
-            yaml.dump(data, f)
-        return get_file_metadata(self.path)
 
 
 @dataclasses.dataclass
@@ -239,8 +193,6 @@ class InMemoryResult(DataSaver):
 
 
 DATA_ADAPTERS = [
-    YAMLDataSaver,
-    YAMLDataLoader,
     JSONDataSaver,
     JSONDataLoader,
     LiteralValueDataLoader,
--- tests/io/test_default_adapters.py
+++ tests/io/test_default_adapters.py
@@ -3,58 +3,9 @@ import json
 import pathlib
 
 import pytest
-import yaml
 
-from hamilton.io.default_data_loaders import (
-    JSONDataLoader,
-    JSONDataSaver,
-    RawFileDataSaverBytes,
-    YAMLDataLoader,
-    YAMLDataSaver,
-)
+from hamilton.io.default_data_loaders import JSONDataLoader, JSONDataSaver, RawFileDataSaverBytes
 from hamilton.io.utils import FILE_METADATA
-
-TEST_DATA_FOR_YAML = [
-    (1, "int.yaml"),
-    ("string", "string.yaml"),
-    (True, "bool.yaml"),
-    ({"key": "value"}, "test.yaml"),
-    ([1, 2, 3], "data.yaml"),
-    ({"nested": {"a": 1, "b": 2}}, "config.yaml"),
-]
-
-
-@pytest.mark.parametrize("data, file_name", TEST_DATA_FOR_YAML)
-def test_yaml_loader(tmp_path: pathlib.Path, data, file_name):
-    path = tmp_path / pathlib.Path(file_name)
-    with path.open(mode="w") as f:
-        yaml.dump(data, f)
-    assert path.exists()
-    loader = YAMLDataLoader(path)
-    loaded_data = loader.load_data(type(data))
-    assert loaded_data[0] == data
-
-
-@pytest.mark.parametrize("data, file_name", TEST_DATA_FOR_YAML)
-def test_yaml_saver(tmp_path: pathlib.Path, data, file_name):
-    path = tmp_path / pathlib.Path(file_name)
-    saver = YAMLDataSaver(path)
-    saver.save_data(data)
-    assert path.exists()
-    with path.open("r") as f:
-        loaded_data = yaml.safe_load(f)
-    assert data == loaded_data
-
-
-@pytest.mark.parametrize("data, file_name", TEST_DATA_FOR_YAML)
-def test_yaml_loader_and_saver(tmp_path: pathlib.Path, data, file_name):
-    path = tmp_path / pathlib.Path(file_name)
-    saver = YAMLDataSaver(path)
-    saver.save_data(data)
-    assert path.exists()
-    loader = YAMLDataLoader(path)
-    loaded_data = loader.load_data(type(data))
-    assert data == loaded_data[0]
 
 
 @pytest.mark.parametrize(
@@ -78,7 +29,13 @@ def test_raw_file_adapter(data, tmp_path: pathlib.Path) -> None:
 
 
 @pytest.mark.parametrize(
-    "data", [{"key": "value"}, [{"key": "value1"}, {"key": "value2"}], ["value1", "value2"], [0, 1]]
+    "data",
+    [
+        {"key": "value"},
+        [{"key": "value1"}, {"key": "value2"}],
+        ["value1", "value2"],
+        [0, 1],
+    ],
 )
 def test_json_save_object_and_array(data, tmp_path: pathlib.Path):
     """Test that `from_.json` and `to.json` can handle JSON objects where
@@ -97,7 +54,13 @@ def test_json_save_object_and_array(data, tmp_path: pathlib.Path):
 
 
 @pytest.mark.parametrize(
-    "data", [{"key": "value"}, [{"key": "value1"}, {"key": "value2"}], ["value1", "value2"], [0, 1]]
+    "data",
+    [
+        {"key": "value"},
+        [{"key": "value1"}, {"key": "value2"}],
+        ["value1", "value2"],
+        [0, 1],
+    ],
 )
 def test_json_load_object_and_array(data, tmp_path: pathlib.Path):
     """Test that `from_.json` and `to.json` can handle JSON objects where
