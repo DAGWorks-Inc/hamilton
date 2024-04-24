035f90018b11ae5b8f5c6fc6b933bcc1f194927b
elijahbenizzy <elijah_ben_izzy@alumni.brown.edu>
2024-04-24 13:55:49 -0700

    Changes port to 8241/8242

--- ui/backend/server/entrypoint.sh
+++ ui/backend/server/entrypoint.sh
@@ -1,4 +1,4 @@
 set -e
 
 python manage.py migrate  # Apply database migrations
-python manage.py runserver 0.0.0.0:8000  # Start the server
+python manage.py runserver 0.0.0.0:8241  # Start the server
--- ui/backend/server/server/default_views.py
+++ ui/backend/server/server/default_views.py
@@ -9,6 +9,7 @@ def root_index(request) -> HttpResponse:
       <header></header>
       <body>
         <h1><center>Welcome to the backend server!</center></h1>
+        <p><center>Did you mean to visit the <a href="http://localhost:8242">frontend</a>? Note this sometimes takes a bit of time to start up.</center> </p>
       </body>
     </html>
     """
