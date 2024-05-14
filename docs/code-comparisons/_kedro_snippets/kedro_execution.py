d519d1eafd4a8e0fdcf2969a79ff75599f615da2
zilto <tjean@DESKTOP-V6JDCS2>
2024-05-14 14:54:55 -0400

    updated comparison

--- docs/code-comparisons/_kedro_snippets/kedro_execution.py
+++ docs/code-comparisons/_kedro_snippets/kedro_execution.py
@@ -1,16 +1,15 @@
 # run.py
-import pandas as pd
-from kedro.io import DataCatalog
 from kedro.runner import SequentialRunner
+from kedro.framework.session import KedroSession
+from kedro.framework.startup import bootstrap_project
 from pipeline import create_pipeline
 # ^ from Step 2
 
+bootstrap_project(".")
+with KedroSession.create() as session:
+    context = session.load_context()
+    catalog = context.catalog
+
 pipeline = create_pipeline().to_nodes("create_model_input_table")
-catalog = DataCatalog(
-    feed_dict=dict(
-        companies=pd.read_parquet("path/to/companies.parquet"),
-        shuttles=pd.read_parquet("path/to/shuttles.parquet"),
-    ),
-)
 SequentialRunner().run(pipeline, catalog)
-# doesn't return a value in-memory
+# doesn't return values in-memory
