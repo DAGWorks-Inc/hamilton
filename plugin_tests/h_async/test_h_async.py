5f291ee134e631f99b6cbc14d16d49a003ae9c84
elijahbenizzy <elijah_ben_izzy@alumni.brown.edu>
2024-06-24 21:56:31 -0700

    Fixes up asynchronous driver for Hamilton
    1. Adds a builder API specifically for the asynchonous driver
    2. Fixes up the ASyncGraphAdapter to call hooks in a clean way
    
    This does something clever -- the graph adapter takes in the hooks,
    which then run async while the function is running. Synchronous hooks
    will work, but they won't be called at quite the right time.

--- plugin_tests/h_async/test_h_async.py
+++ plugin_tests/h_async/test_h_async.py
@@ -6,6 +6,18 @@ import pytest
 
 from hamilton import base
 from hamilton.experimental import h_async
+from hamilton.lifecycle.base import (
+    BasePostGraphConstruct,
+    BasePostGraphConstructAsync,
+    BasePostGraphExecute,
+    BasePostGraphExecuteAsync,
+    BasePostNodeExecute,
+    BasePostNodeExecuteAsync,
+    BasePreGraphExecute,
+    BasePreGraphExecuteAsync,
+    BasePreNodeExecute,
+    BasePreNodeExecuteAsync,
+)
 
 from .resources import simple_async_module
 
@@ -102,3 +114,139 @@ async def test_driver_end_to_end_telemetry(send_event_json):
     await asyncio.gather(*[t for t in tasks if t != current_task])
     assert send_event_json.called
     assert len(send_event_json.call_args_list) == 2
+
+
+@pytest.mark.asyncio
+async def test_async_driver_end_to_end_async_lifecycle_methods():
+    tracked_calls = []
+
+    class AsyncTrackingAdapter(
+        BasePostGraphConstructAsync,
+        BasePreGraphExecuteAsync,
+        BasePostGraphExecuteAsync,
+        BasePreNodeExecuteAsync,
+        BasePostNodeExecuteAsync,
+    ):
+        def __init__(self, calls: list, pause_time: float = 0.01):
+            self.pause_time = pause_time
+            self.calls = calls
+
+        async def _pause(self):
+            return await asyncio.sleep(self.pause_time)
+
+        async def pre_graph_execute(self, **kwargs):
+            await self._pause()
+            self.calls.append(("pre_graph_execute", kwargs))
+
+        async def post_graph_execute(self, **kwargs):
+            await self._pause()
+            self.calls.append(("post_graph_execute", kwargs))
+
+        async def pre_node_execute(self, **kwargs):
+            await self._pause()
+            self.calls.append(("pre_node_execute", kwargs))
+
+        async def post_node_execute(self, **kwargs):
+            await self._pause()
+            self.calls.append(("post_node_execute", kwargs))
+
+        async def post_graph_construct(self, **kwargs):
+            await self._pause()
+            self.calls.append(("post_graph_construct", kwargs))
+
+    adapter = AsyncTrackingAdapter(tracked_calls)
+
+    dr = await h_async.AsyncDriver(
+        {}, simple_async_module, result_builder=base.DictResult(), adapters=[adapter]
+    ).ainit()
+    all_vars = [var.name for var in dr.list_available_variables() if var.name != "return_df"]
+    result = await dr.execute(final_vars=all_vars, inputs={"external_input": 1})
+    hooks_called = [call[0] for call in tracked_calls]
+    assert set(hooks_called) == {
+        "pre_graph_execute",
+        "post_graph_execute",
+        "pre_node_execute",
+        "post_node_execute",
+        "post_graph_construct",
+    }
+    result["a"] = result["a"].to_dict()
+    result["b"] = result["b"].to_dict()
+    assert result == {
+        "a": pd.Series([1, 2, 3]).to_dict(),
+        "another_async_func": 8,
+        "async_func_with_param": 4,
+        "b": pd.Series([4, 5, 6]).to_dict(),
+        "external_input": 1,
+        "non_async_func_with_decorator": {"result_1": 9, "result_2": 5},
+        "result_1": 9,
+        "result_2": 5,
+        "result_3": 1,
+        "result_4": 2,
+        "return_dict": {"result_3": 1, "result_4": 2},
+        "simple_async_func": 2,
+        "simple_non_async_func": 7,
+    }
+
+
+@pytest.mark.asyncio
+async def test_async_driver_end_to_end_sync_lifecycle_methods():
+    tracked_calls = []
+
+    class AsyncTrackingAdapter(
+        BasePostGraphConstruct,
+        BasePreGraphExecute,
+        BasePostGraphExecute,
+        BasePreNodeExecute,
+        BasePostNodeExecute,
+    ):
+        def __init__(self, calls: list, pause_time: float = 0.01):
+            self.pause_time = pause_time
+            self.calls = calls
+
+        def pre_graph_execute(self, **kwargs):
+            self.calls.append(("pre_graph_execute", kwargs))
+
+        def post_graph_execute(self, **kwargs):
+            self.calls.append(("post_graph_execute", kwargs))
+
+        def pre_node_execute(self, **kwargs):
+            self.calls.append(("pre_node_execute", kwargs))
+
+        def post_node_execute(self, **kwargs):
+            self.calls.append(("post_node_execute", kwargs))
+
+        def post_graph_construct(self, **kwargs):
+            self.calls.append(("post_graph_construct", kwargs))
+
+    adapter = AsyncTrackingAdapter(tracked_calls)
+
+    dr = await h_async.AsyncDriver(
+        {}, simple_async_module, result_builder=base.DictResult(), adapters=[adapter]
+    ).ainit()
+    all_vars = [var.name for var in dr.list_available_variables() if var.name != "return_df"]
+    result = await dr.execute(final_vars=all_vars, inputs={"external_input": 1})
+    hooks_called = [call[0] for call in tracked_calls]
+    assert set(hooks_called) == {
+        "pre_graph_execute",
+        "post_graph_execute",
+        "pre_node_execute",
+        "post_node_execute",
+        "post_graph_construct",
+    }
+    result["a"] = result["a"].to_dict()
+    result["b"] = result["b"].to_dict()
+    assert result == {
+        "a": pd.Series([1, 2, 3]).to_dict(),
+        "another_async_func": 8,
+        "async_func_with_param": 4,
+        "b": pd.Series([4, 5, 6]).to_dict(),
+        "external_input": 1,
+        "non_async_func_with_decorator": {"result_1": 9, "result_2": 5},
+        "result_1": 9,
+        "result_2": 5,
+        "result_3": 1,
+        "result_4": 2,
+        "return_dict": {"result_3": 1, "result_4": 2},
+        "simple_async_func": 2,
+        "simple_non_async_func": 7,
+    }
