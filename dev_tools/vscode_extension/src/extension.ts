1204055e2730e26fc8a67b64fc531a0fc26a515e
zilto <tjean@DESKTOP-V6JDCS2>
2024-06-27 12:20:18 -0400

    improved installation process

--- dev_tools/vscode_extension/src/extension.ts
+++ dev_tools/vscode_extension/src/extension.ts
@@ -1,7 +1,32 @@
 import * as vscode from "vscode";
+import { execSync } from "child_process";
 import { DataflowWebviewFeature } from "./features/dataflowWebviewFeature";
-import { LSPClientFeature, startLspServer } from "./features/lspClientFeature";
+import { LSPClientFeature } from "./features/lspClientFeature";
 import { SupportLinksFeature } from "./features/supportLinksFeature";
+
+
+const checkPythonDependencies = (pythonPath: string) => {
+  try{
+    execSync(`${pythonPath} -c "import hamilton_lsp"`)
+    return true
+  } catch (error) {
+    return false
+  }
+}
+
+function installDependenciesMessage(pythonPath: string) {
+  let installLSP = "Install sf-hamilton[lsp]"
+
+  vscode.window.showInformationMessage("Missing the Hamilton language server.", installLSP)
+    .then(selection => {
+      if (selection === installLSP) {
+        execSync(`${pythonPath} -m pip install 'sf-hamilton-lsp'`)
+        vscode.commands.executeCommand("workbench.action.reloadWindow")
+      }
+    })
+}
+
+
 
 let extensionFeatures: any[];
 
@@ -9,9 +34,17 @@ export async function activate(context: vscode.ExtensionContext) {
   const pythonExtension = vscode.extensions.getExtension("ms-python.python");
   const pythonPath = pythonExtension?.exports.settings.getExecutionDetails().execCommand?.join("");
 
-  const lspServer: LSPClientFeature | undefined = startLspServer(context, pythonPath);
+  const available = checkPythonDependencies(pythonPath)
+  if (available === false) {
+    installDependenciesMessage(pythonPath)
+    return
+  }
 
-  extensionFeatures = [lspServer, new DataflowWebviewFeature(context), new SupportLinksFeature()];
+  extensionFeatures = [
+    new LSPClientFeature(context, pythonPath),
+    new DataflowWebviewFeature(context),
+    new SupportLinksFeature()
+  ];
 }
 
 export function deactivate() {
