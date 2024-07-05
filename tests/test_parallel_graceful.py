17ffb36c10bd484d19616656f52789a4adbc2814
James Arruda <james.arruda@gtri.gatech.edu>
2024-07-05 13:12:30 -0400

    Modifying GracefulErrorAdpater for Parallelizable blocks.

--- tests/test_parallel_graceful.py
+++ tests/test_parallel_graceful.py
@@ -1,13 +1,28 @@
 from hamilton import ad_hoc_utils, driver
 from hamilton.execution.executors import SynchronousLocalTaskExecutor
+from hamilton.function_modifiers import config
 from hamilton.htypes import Collect, Parallelizable
 from hamilton.lifecycle import GracefulErrorAdapter
 
 
-def distro(n: int, allow_fail: bool) -> Parallelizable[int]:
+@config.when(test_state="middle")
+def distro__middle(n: int) -> Parallelizable[int]:
     for x in range(n):
-        if x > 4 and allow_fail:
+        if x > 4:
             raise Exception("bad")
+        yield x * 3
+
+
+@config.when(test_state="early")
+def distro__early(n: int) -> Parallelizable[int]:
+    raise Exception("bad")
+    for x in range(n):
+        yield x * 3
+
+
+@config.when(test_state="pass")
+def distro__pass(n: int) -> Parallelizable[int]:
+    for x in range(n):
         yield x * 3
 
 
@@ -22,14 +37,20 @@ def distro_gather(some_math: Collect[float]) -> list[float]:
     return ans
 
 
-def test_parallel_error() -> None:
+# TODO: How does this break extract_fields, e.g.?
+
+temp_module = ad_hoc_utils.create_temporary_module(
+    distro__early,
+    distro__middle,
+    distro__pass,
+    some_math,
+    distro_gather,
+    module_name="parallel_example",
+)
+
+
+def _make_driver(test_state: str, fail_parallel=False):
     local_executor = SynchronousLocalTaskExecutor()
-    temp_module = ad_hoc_utils.create_temporary_module(
-        distro,
-        some_math,
-        distro_gather,
-        module_name="parallel_example",
-    )
     dr = (
         driver.Builder()
         .enable_dynamic_execution(allow_experimental_mode=True)
@@ -39,14 +60,46 @@ def test_parallel_error() -> None:
             GracefulErrorAdapter(
                 error_to_catch=Exception,
                 sentinel_value=None,
+                fail_all_parallel=fail_parallel,
             )
+        )
+        .with_config(
+            {"test_state": test_state},
         )
         .build()
     )
-    _ = dr.execute(
+    return dr
+
+
+def test_parallel_error() -> None:
+    # Fail before the anything is yielded: should get 1 failure and skip all
+    dr = _make_driver("early")
+    ans = dr.execute(
         ["distro_gather"],
-        inputs={
-            "n": 10,
-            "allow_fail": True,
-        },
+        inputs={"n": 10},
     )
+    assert ans["distro_gather"] == [None]
+
+    # Fail in the middle of the parallel: ends early
+    dr = _make_driver("middle")
+    ans = dr.execute(
+        ["distro_gather"],
+        inputs={"n": 10},
+    )
+    assert ans["distro_gather"] == [0.0, 6.0, 12.0, 18.0, 24.0, None]
+
+    # Fail in the middle of the parallel, but we tell it to act as 1 failure
+    dr = _make_driver("middle", True)
+    ans = dr.execute(
+        ["distro_gather"],
+        inputs={"n": 10},
+    )
+    assert ans["distro_gather"] == [None]
+
+    # Parallelized block doesn't fail, we see the collect work on the internal failures
+    dr = _make_driver("pass")
+    ans = dr.execute(
+        ["distro_gather"],
+        inputs={"n": 10},
+    )
+    assert ans["distro_gather"] == [0.0, 6.0, 12.0, 18.0, 24.0, 30.0, None, None, None, None]
