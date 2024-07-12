ecc93376068e73b26e89e016f736367d747d02e1
elijahbenizzy <elijah_ben_izzy@alumni.brown.edu>
2024-07-11 17:01:30 -0700

    Gets the schema view to use GenericGroupedTable
    This will make it look like the others, make it expandable, etc...

--- ui/frontend/src/components/dashboard/Runs/Task/result-summaries/SchemaView.tsx
+++ ui/frontend/src/components/dashboard/Runs/Task/result-summaries/SchemaView.tsx
@@ -1,82 +1,142 @@
 import { AttributeSchema1 } from "../../../../../state/api/friendlyApi";
-import { GenericTable } from "../../../../common/GenericTable";
-import {ColSchema} from "../../../../../state/api/backendApiRaw";
-
-
-// export const Schema1View = (props: {
-//     taskName: string;
-//     runIds: number[];
-//     values: AttributeSchema1[];
-//   }) => {
-//     const columns = [
-//         {
-//             displayName: "ColName",
-//             Render: (item: AttributeSchema1) => <span>{item.name}</span>,
-//         },
-//         {
-//             displayName: "Name",
-//             Render: (item: AttributeSchema1) => <span>{item.name}</span>,
-//         },
-//         {
-//             displayName: "Value",
-//             Render: (item: AttributeSchema1) => <span>{item.value}</span>,
-//         },
-//   ];
-//     const data = props.values.map(value => [value.id, value.name, value.value]);
-//     return (
-// <div>hi</div>
-//     );
-//   };
+import { GenericGroupedTable } from "../../../../common/GenericTable";
+import { ColSchema } from "../../../../../state/api/backendApiRaw";
+import JsonView from "@uiw/react-json-view";
+import { RunLink } from "../../../../common/CommonLinks";
 
 interface Schema1ViewProps {
   attributeSchemas: AttributeSchema1[];
-  taskName: string;
   runIds: number[];
+  projectId: number;
 }
-// export type RunIdToSchema = {
-//   [key: number]: AttributeSchema1;
-// };
 
-// const buildRunIdToAttributeSchemaMapping = (props: Schema1ViewProps): { [key: number]: AttributeSchema1 } => {
-//   const mapping: RunIdToSchema = {};
-//
-//   props.runIds.forEach((runId, index) => {
-//     mapping[runId] = props.attributeSchemas[index];
-//   });
-//
-//   return mapping;
-// };
+type AttributeColumn = ColSchema & {
+  runId: number;
+  projectId: number;
+};
 
-export const Schema1View: React.FC<Schema1ViewProps> = ({ attributeSchemas, taskName, runIds }) => {
-    // Convert the AttributeSchema1 object into an array of its values
-    // const runIdtoAttributeSchema = buildRunIdToAttributeSchemaMapping({ attributeSchemas, taskName, runIds });
-    const schemaArray = Object.values(attributeSchemas[0]);
-    console.log(attributeSchemas);
-    console.log(runIds);
+export const Schema1View: React.FC<Schema1ViewProps> = ({
+  attributeSchemas,
+  runIds,
+  projectId,
+}) => {
+  const attributeColumns: AttributeColumn[] = [];
 
-    // return (
-    //     <GenericTable data={} columns={} dataTypeName={}></GenericTable>
-    // )
-    return (
-        <table>
-            <thead>
-            <tr>
-                <th>Column Name</th>
-                <th>Type</th>
-                <th>Nullable</th>
-                <th>Metadata</th>
-            </tr>
-            </thead>
-            <tbody>
-            {schemaArray.map((col, index) => (
-                <tr key={index}>
-                    <td>{col.name}</td>
-                    <td>{col.type}</td>
-                    <td>{col.nullable}</td>
-                    <td>{JSON.stringify(col.metadata)}</td>
-                </tr>
+  attributeSchemas.forEach((schema, i) => {
+    const runId = runIds[i];
+    Object.keys(schema).forEach((key) => {
+      attributeColumns.push({
+        runId: runId,
+        projectId: projectId,
+        ...schema[key],
+      });
+    });
+  });
+
+  const columns = [
+    {
+      displayName: "runs",
+      Render: (
+        value: { runId: number }[],
+        isSummaryRow: boolean,
+        isExpanded: boolean
+      ) => {
+        const runIds = value.map((item) => item.runId);
+        if (isSummaryRow && isExpanded) {
+          return <></>;
+        }
+        return (
+          <div className="flex flex-row gap-1">
+            {runIds.map((taskRun, i) => (
+              <RunLink
+                runId={runIds[i]}
+                key={i}
+                projectId={1}
+                highlightedRun={null}
+                setHighlightedRun={() => void 0}
+              />
             ))}
-            </tbody>
-        </table>
-    );
-}
+          </div>
+        );
+      },
+    },
+
+    {
+      displayName: "type",
+      Render: (
+        value: AttributeColumn[],
+        isSummaryRow: boolean,
+        isExpanded: boolean
+      ) => {
+        const toDisplay = Array.from(new Set(value.map((item) => item.type)));
+        console.log(value);
+        return !isSummaryRow || !isExpanded ? (
+          <div className="flex flex-wrap gap-2">
+            {toDisplay.map((item, i) => (
+              <code key={i}>{item}</code>
+            ))}
+          </div>
+        ) : (
+          <></>
+        );
+      },
+    },
+    {
+      displayName: "nullable",
+      Render: (
+        value: AttributeColumn[],
+        isSummaryRow: boolean,
+        isExpanded: boolean
+      ) => {
+        console.log(value);
+        const allExpanded = value.map((item) => item.nullable);
+        console.log(allExpanded);
+
+        return !isSummaryRow || !isExpanded ? (
+          <div className="flex flex-wrap gap-2">
+            {allExpanded.map((item) => (item ? "✓" : "✗"))}
+          </div>
+        ) : (
+          <></>
+        );
+      },
+    },
+    {
+      displayName: "metadata",
+      Render: (
+        value: AttributeColumn[],
+        isSummaryRow: boolean,
+        isExpanded: boolean
+      ) => {
+        const toDisplay = Array.from(
+          new Set(value.map((item) => item.metadata))
+        );
+        console.log(value);
+        return !isSummaryRow || !isExpanded ? (
+          <div className="flex flex-wrap gap-2">
+            {toDisplay.map((item, i) => (
+              <JsonView
+                key={i}
+                value={item}
+                collapsed={2}
+                enableClipboard={false}
+              />
+            ))}
+          </div>
+        ) : (
+          <></>
+        );
+      },
+    },
+  ];
+  return (
+    <div className="py-10">
+      <GenericGroupedTable
+        dataTypeName="column"
+        data={attributeColumns.map((col) => [col.name, col])}
+        // description="Numeric columns in the dataset"
+        columns={columns}
+      />
+    </div>
+  );
+};
