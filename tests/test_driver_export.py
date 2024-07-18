52ecf5f23e204e22e2fbd5acbdcf900515ad2561
Stefan Krawczyk <stefan@dagworks.io>
2024-07-18 13:28:53 -0700

    Implements #710 optional value exposure
    This fixes #710.
    
    This hasn't been extensively tested, but this
    seems to work and pass things through as
    expected. There is a JSON serialization
    caveat, however we expect default
    values to be primitives so that should
    be fine. If not the user can augment the
    dictionary returned ...

--- tests/test_driver_export.py
+++ tests/test_driver_export.py
@@ -13,13 +13,10 @@ EXPECTED_JSON = {
             "output_type": "int",
             "required_dependencies": ["sum_step_squared_plus_step_cubed"],
             "optional_dependencies": [],
-            "source": (
-                "def final(sum_step_squared_plus_step_cubed: int) -> int:\n"
-                '    print("finalizing")\n'
-                "    return sum_step_squared_plus_step_cubed\n"
-            ),
+            "optional_input_values": {},
+            "source": 'def final(sum_step_squared_plus_step_cubed: int) -> int:\n    print("finalizing")\n    return sum_step_squared_plus_step_cubed\n',
             "documentation": "",
-            "version": None,  # `version` value isn't hardcoded because it's Python version-dependent
+            "version": "27af6c2870dd4ca92335df4a9ff6c84da829d792a4c9f690f2f57193dbf3395a",
         },
         {
             "name": "number_of_steps",
@@ -27,9 +24,10 @@ EXPECTED_JSON = {
             "output_type": "int",
             "required_dependencies": [],
             "optional_dependencies": [],
+            "optional_input_values": {},
             "source": "def number_of_steps() -> int:\n    return 5\n",
             "documentation": "",
-            "version": None,
+            "version": "48945782868c44bca1b159682d48bee3a1c1e9be73cf059c5087d0d3258fdc78",
         },
         {
             "name": "step_cubed",
@@ -37,13 +35,10 @@ EXPECTED_JSON = {
             "output_type": "int",
             "required_dependencies": ["steps"],
             "optional_dependencies": [],
-            "source": (
-                "def step_cubed(steps: int) -> int:\n"
-                '    print("cubing step {}".format(steps))\n'
-                "    return steps**3\n"
-            ),
+            "optional_input_values": {},
+            "source": 'def step_cubed(steps: int) -> int:\n    print("cubing step {}".format(steps))\n    return steps**3\n',
             "documentation": "",
-            "version": None,
+            "version": "f82b894f3d8a8de9cf798a41e08c1f38c8396e86c569cb73b7d78b4420f9dd99",
         },
         {
             "name": "step_squared",
@@ -51,10 +46,10 @@ EXPECTED_JSON = {
             "output_type": "int",
             "required_dependencies": ["steps"],
             "optional_dependencies": [],
-            "source": 'def step_squared(steps: int) -> int:\n    print("squaring step {}".format(steps))\n   '
-            " return steps**2\n",
+            "optional_input_values": {},
+            "source": 'def step_squared(steps: int) -> int:\n    print("squaring step {}".format(steps))\n    return steps**2\n',
             "documentation": "",
-            "version": None,
+            "version": "371bff5e02e091314e0ef2c3ca3003a496a119f9164d3e1827fe64d5f8c4aaba",
         },
         {
             "name": "step_squared_plus_step_cubed",
@@ -62,13 +57,10 @@ EXPECTED_JSON = {
             "output_type": "int",
             "required_dependencies": ["step_cubed", "step_squared"],
             "optional_dependencies": [],
-            "source": (
-                "def step_squared_plus_step_cubed(step_squared: int, step_cubed: int) -> int:\n"
-                '    print("adding step squared and step cubed")\n'
-                "    return step_squared + step_cubed\n"
-            ),
+            "optional_input_values": {},
+            "source": 'def step_squared_plus_step_cubed(step_squared: int, step_cubed: int) -> int:\n    print("adding step squared and step cubed")\n    return step_squared + step_cubed\n',
             "documentation": "",
-            "version": None,
+            "version": "fe76eaaf43a6b79a706574ea6a8f00fe5b6439e3b8d7ceb879ab938b7722a1b3",
         },
         {
             "name": "steps",
@@ -76,9 +68,10 @@ EXPECTED_JSON = {
             "output_type": "int",
             "required_dependencies": ["number_of_steps"],
             "optional_dependencies": [],
+            "optional_input_values": {},
             "source": "def steps(number_of_steps: int) -> int:\n    return number_of_steps\n",
             "documentation": "",
-            "version": None,
+            "version": "111ccfe2b5e8754e5ada1dae3ffc49d27b450a98e3be61f9308144aa5a444921",
         },
         {
             "name": "sum_step_squared_plus_step_cubed",
@@ -86,15 +79,12 @@ EXPECTED_JSON = {
             "output_type": "int",
             "required_dependencies": ["step_squared_plus_step_cubed"],
             "optional_dependencies": [],
-            "source": (
-                "def sum_step_squared_plus_step_cubed(step_squared_plus_step_cubed: int) -> int:\n"
-                '    print("summing step squared")\n'
-                "    return step_squared_plus_step_cubed\n"
-            ),
+            "optional_input_values": {},
+            "source": 'def sum_step_squared_plus_step_cubed(step_squared_plus_step_cubed: int) -> int:\n    print("summing step squared")\n    return step_squared_plus_step_cubed\n',
             "documentation": "",
-            "version": None,
+            "version": "7831c9ed5f45e0b8ce3de34c3a5e2b4d4467b1ebc44cb55ea6ecae8f7dbf749d",
         },
-    ],
+    ]
 }
 
 
