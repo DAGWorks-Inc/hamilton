45149215245bc47656bd7678de9053df4e21e183
Stefan Krawczyk <stefan@dagworks.io>
2024-07-22 12:50:13 -0700

    Fixes test for 3.8

--- hamilton/function_modifiers/adapters.py
+++ hamilton/function_modifiers/adapters.py
@@ -746,7 +746,8 @@ class dataloader(NodeCreator):
                 f"Function: {fn.__qualname__} must return a tuple of length 2."
             )
         # check that the second is a dict
-        if not typing_inspect.get_args(return_annotation)[1] == dict:
+        second_arg = typing_inspect.get_args(return_annotation)[1]
+        if not second_arg == dict or not second_arg == Dict:
             raise InvalidDecoratorException(
                 f"Function: {fn.__qualname__} must return a tuple of type (SOME_TYPE, dict)."
             )
