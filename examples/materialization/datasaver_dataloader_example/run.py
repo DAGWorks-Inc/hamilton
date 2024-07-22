5c2a0f6d6866ee9278282d0fcd00690332a633d3
Stefan Krawczyk <stefan@dagworks.io>
2024-07-22 11:40:05 -0700

    Adds example README

--- examples/materialization/datasaver_dataloader_example/run.py
+++ examples/materialization/datasaver_dataloader_example/run.py
@@ -5,12 +5,10 @@ from hamilton import driver
 
 tracker = adapters.HamiltonTracker(
     project_id=7,  # modify this as needed
-    username="elijah@dagworks.io",
-    dag_name="my_version_of_the_dag",
+    username="elijah@dagworks.io",  # modify this as needed
+    dag_name="my_example_dag",
     tags={"environment": "DEV", "team": "MY_TEAM", "version": "X"},
-)  # note this slows down execution because there's 60 columns.
-# 30 columns adds about a 1 second.
-# 60 is therefore 2 seconds.
+)
 
 dr = driver.Builder().with_config({}).with_modules(simple_etl).with_adapters(tracker).build()
 dr.display_all_functions("simple_etl.png")
