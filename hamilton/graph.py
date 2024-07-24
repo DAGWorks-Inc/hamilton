088831d97c17b4b539e9a25d861f40eb45cd78ac
Stefan Krawczyk <stefan@dagworks.io>
2024-07-24 00:00:45 -0700

    Attempt at fixing visualization for pyodide
    this seems to work, but then it doesnt...

--- hamilton/graph.py
+++ hamilton/graph.py
@@ -10,7 +10,8 @@ import html
 import inspect
 import logging
 import os.path
-import pathlib
+
+# import pathlib
 import uuid
 from enum import Enum
 from types import ModuleType
@@ -938,13 +939,15 @@ class FunctionGraph:
         # - `render()` accepts a `view` kwarg
         # - `render()` will append it's kwarg `format` to the filename
         if output_file_path:
-            if keep_dot:
-                kwargs["view"] = kwargs.get("view", False)
-                dot.render(output_file_path, **kwargs)
-            else:
-                kwargs.pop("view", None)
-                output_file_path = f"{output_file_path}.{kwargs['format']}"
-                pathlib.Path(output_file_path).write_bytes(dot.pipe(**kwargs))
+            with open(output_file_path + ".dot", "w") as f:
+                f.write(str(dot))
+            # if keep_dot:
+            #     kwargs["view"] = kwargs.get("view", False)
+            #     dot.render(output_file_path, **kwargs)
+            # else:
+            #     kwargs.pop("view", None)
+            #     output_file_path = f"{output_file_path}.{kwargs['format']}"
+            #     pathlib.Path(output_file_path).write_bytes(dot.pipe(**kwargs))
         return dot
 
     def get_impacted_nodes(self, var_changes: List[str]) -> Set[node.Node]:
