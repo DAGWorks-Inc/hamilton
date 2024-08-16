c1a3c0834986c02aca9254964079507409577c8a
zilto <tjean@DESKTOP-V6JDCS2>
2024-08-16 14:56:50 -0400

    removed test that messed with sys.modules

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -1,27 +1,6 @@
-import os
-import sys
-
 import pytest
 
 from hamilton import registry
-
-
-def test_disable_autoload_then_reenable():
-    os.environ[registry.HAMILTON_AUTOLOAD_ENV] = "0"
-    # remove pandas from the loaded modules
-    # pandas is both a Hamilton dependency and it has pandas_extensions.py
-    pandas_modules = [module_name for module_name in sys.modules if "pandas" in module_name]
-    for module_name in pandas_modules:
-        sys.modules.pop(module_name)
-
-    # pandas shouldn't be autoloaded
-    registry.initialize()
-    assert "pandas" not in sys.modules
-
-    # pandas should be autoloaded
-    registry.enable_autoload()
-    registry.initialize()
-    assert "pandas" in sys.modules
 
 
 @pytest.mark.parametrize("entrypoint", ["config_disable_autoload", "config_enable_autoload"])
