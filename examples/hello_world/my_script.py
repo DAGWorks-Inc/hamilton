8dd672a8d7c59fbf11b6a3da8d79434d57e309ea
elijahbenizzy <elijah_ben_izzy@alumni.brown.edu>
2024-08-26 11:23:07 -0700

    Fixes up the hello_world script
    This is a remnant of the past -- this takes away all the ways to do
    things, and simplifies the explanation

--- examples/hello_world/my_script.py
+++ examples/hello_world/my_script.py
@@ -1,43 +1,40 @@
-# import importlib
 import logging
 import sys
 
 import pandas as pd
 
-from hamilton import driver
+from hamilton import base, driver
 
 logging.basicConfig(stream=sys.stdout)
-initial_columns = {  # load from actuals or wherever -- this is our initial data we use as input.
-    # Note: these values don't have to be all series, they could be a scalar.
-    "signups": pd.Series([1, 10, 50, 100, 200, 400]),
-    "spend": pd.Series([10, 10, 20, 40, 40, 50]),
-}
+
 # we need to tell hamilton where to load function definitions from
-# programmatic code to load modules:
-# module_name = "my_functions"
-# my_functions = importlib.import_module(module_name)
-# or import module(s) directly:
+# we use module pointers to do this
 import my_functions
 
-dr = driver.Driver(initial_columns, my_functions)  # can pass in multiple modules
-# we need to specify what we want in the final dataframe. These can be string names, or function references.
+# This uses the module above, but you can pass in as many as you want
+# In this case we want a dataframe output, but there are a host of other options
+dr = driver.Builder().with_modules(my_functions).with_adapters(base.PandasDataFrameResult()).build()
+
+# we need to specify what we want in the final results
+# These can be string names or function pointers (E.G. my_functions.spend_per_signup)
 output_columns = [
     "spend",
     "signups",
-    my_functions.avg_3wk_spend,  # could just pass "avg_3wk_spend" here
-    my_functions.spend_per_signup,  # could just pass "spend_per_signup" here
-    my_functions.spend_zero_mean_unit_variance,  # could just pass "spend_zero_mean_unit_variance" here
+    "avg_3wk_spend",
+    "spend_per_signup",
+    "spend_zero_mean_unit_variance",
 ]
-# let's create the dataframe!
-df = dr.execute(output_columns)
+
+# We can also load these up from wherever we want, or even do so inside the
+# DAG. For now we just hardcode them.
+initial_columns = {
+    "signups": pd.Series([1, 10, 50, 100, 200, 400]),
+    "spend": pd.Series([10, 10, 20, 40, 40, 50]),
+}
+# let's compute the results!
+df = dr.execute(output_columns, inputs=initial_columns)
 print(df.to_string())
 
 # To visualize do `pip install "sf-hamilton[visualization]"` if you want these to work
-dr.visualize_execution(output_columns, "./my_dag.png")
-dr.visualize_path_between(
-    "spend_mean",
-    "spend_zero_mean_unit_variance",
-    "./a_path.png",
-    strict_path_visualization=False,
-)
-dr.display_all_functions("./my_full_dag.png")
+# Also look up visualize_path_between and visualize_execution
+dr.display_all_functions("./my_dag.png")
