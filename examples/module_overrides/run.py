7200720a7cdf6873f8292aa09cdb532ef0674911
jernejfrank <jernejfrank@gmail.com>
2024-09-11 22:28:07 +0100

    sketch implemented that works

--- examples/module_overrides/run.py
+++ examples/module_overrides/run.py
@@ -1,4 +1,6 @@
 import module_a
+import module_b
+import module_c
 
 from hamilton import driver
 
@@ -7,9 +9,14 @@ if __name__ == "__main__":
         driver.Builder()
         .with_modules(
             module_a,
-            #   module_b
+            module_b,
+            module_c,
         )
+        .allow_module_overrides()
         .build()
     )
 
-    print(dr.execute(inputs={}, final_vars=["foo"]))
+    print("builder: ", dr.execute(inputs={}, final_vars=["foo"]))
+
+    dr2 = driver.Driver({}, *[module_a, module_b], allow_module_overrides=True)
+    print("driver: ", dr2.execute(inputs={}, final_vars=["foo"]))
