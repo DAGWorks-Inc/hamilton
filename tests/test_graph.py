0c94426e82f70ba9bc188cc609c66a82787d595c
jernejfrank <jernejfrank@gmail.com>
2024-09-12 20:06:58 +0100

    Added test

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -20,6 +20,7 @@ import tests.resources.compatible_input_types
 import tests.resources.config_modifier
 import tests.resources.cyclic_functions
 import tests.resources.dummy_functions
+import tests.resources.dummy_functions_module_override
 import tests.resources.extract_column_nodes
 import tests.resources.extract_columns_execution_count
 import tests.resources.functions_with_generics
@@ -455,11 +456,70 @@ def create_testing_nodes():
     return nodes
 
 
+def create_testing_nodes_override_B():
+    """Helper function for creating the nodes represented in dummy_functions.py
+    with node B overridden by dummy_functions_module_override.py."""
+    nodes = {
+        "A": node.Node(
+            "A",
+            inspect.signature(tests.resources.dummy_functions.A).return_annotation,
+            "Function that should become part of the graph - A",
+            tests.resources.dummy_functions.A,
+            tags={"module": "tests.resources.dummy_functions"},
+        ),
+        "B": node.Node(
+            "B",
+            inspect.signature(tests.resources.dummy_functions_module_override.B).return_annotation,
+            "Function that should override function B.",
+            tests.resources.dummy_functions_module_override.B,
+            tags={"module": "tests.resources.dummy_functions_module_override"},
+        ),
+        "C": node.Node(
+            "C",
+            inspect.signature(tests.resources.dummy_functions.C).return_annotation,
+            "",
+            tests.resources.dummy_functions.C,
+            tags={"module": "tests.resources.dummy_functions"},
+        ),
+        "b": node.Node(
+            "b",
+            inspect.signature(tests.resources.dummy_functions.A).parameters["b"].annotation,
+            node_source=NodeType.EXTERNAL,
+        ),
+        "c": node.Node(
+            "c",
+            inspect.signature(tests.resources.dummy_functions.A).parameters["c"].annotation,
+            node_source=NodeType.EXTERNAL,
+        ),
+    }
+    nodes["A"].dependencies.append(nodes["b"])
+    nodes["A"].dependencies.append(nodes["c"])
+    nodes["A"].depended_on_by.append(nodes["B"])
+    nodes["A"].depended_on_by.append(nodes["C"])
+    nodes["b"].depended_on_by.append(nodes["A"])
+    nodes["c"].depended_on_by.append(nodes["A"])
+    nodes["B"].dependencies.append(nodes["A"])
+    nodes["C"].dependencies.append(nodes["A"])
+    return nodes
+
+
 def test_create_function_graph_simple():
     """Tests that we create a simple function graph."""
     expected = create_testing_nodes()
     actual = graph.create_function_graph(tests.resources.dummy_functions, config={})
     assert actual == expected
+
+
+def test_create_function_graph_with_override():
+    """Tests that we can override nodes from later modules in function graph."""
+    override_expected = create_testing_nodes_override_B()
+    override_actual = graph.create_function_graph(
+        tests.resources.dummy_functions,
+        tests.resources.dummy_functions_module_override,
+        config={},
+        allow_module_overrides=True,
+    )
+    assert override_expected == override_actual
 
 
 def test_execute():
