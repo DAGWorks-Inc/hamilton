1eb9953b098201ebaa6c07a9920f24299b1d033e
sikehish <hisham0502@gmail.com>
2024-10-17 20:54:41 +0530

    Created buildx_and_push.sh script in ui directory to create multi-platform docker builds

---
+++ ui/buildx_and_push.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
+# check if Buildx is already enabled; create a builder instance if not
+docker buildx inspect hamilton-builder > /dev/null 2>&1 || \
+    docker buildx create --use --name hamilton-builder
+
+FRONTEND_IMAGE="dagworks/ui-frontend"
+BACKEND_IMAGE="dagworks/ui-backend"
+VERSION="latest" #can be modified to a desired version
+
+docker buildx build --platform linux/amd64,linux/arm64 \
+    -t $BACKEND_IMAGE:$VERSION -t $BACKEND_IMAGE:latest \
+    --push -f backend/Dockerfile.backend .
+
+docker buildx build --platform linux/amd64,linux/arm64 \
+    -t $FRONTEND_IMAGE:$VERSION -t $FRONTEND_IMAGE:latest \
+    --push -f frontend/Dockerfile.frontend .
