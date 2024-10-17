5ce6bdac57d3909a7ddc80927e23ea6702017c71
sikehish <hisham0502@gmail.com>
2024-10-17 22:17:23 +0530

    Fixed content related issue in buildx_and_push.sh

--- ui/buildx_and_push.sh
+++ ui/buildx_and_push.sh
@@ -2,18 +2,18 @@
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
-# check if Buildx is already enabled; create a builder instance if not
+# Check if Buildx is already enabled; create a builder instance if not
 docker buildx inspect hamilton-builder > /dev/null 2>&1 || \
     docker buildx create --use --name hamilton-builder
 
 FRONTEND_IMAGE="dagworks/ui-frontend"
 BACKEND_IMAGE="dagworks/ui-backend"
-VERSION="latest" #can be modified to a desired version
+VERSION="latest" # can be modified to a desired version
 
 docker buildx build --platform linux/amd64,linux/arm64 \
     -t $BACKEND_IMAGE:$VERSION -t $BACKEND_IMAGE:latest \
-    --push -f backend/Dockerfile.backend .
+    --push -f backend/Dockerfile.backend backend/
 
 docker buildx build --platform linux/amd64,linux/arm64 \
     -t $FRONTEND_IMAGE:$VERSION -t $FRONTEND_IMAGE:latest \
-    --push -f frontend/Dockerfile.frontend .
+    --push -f frontend/Dockerfile.frontend frontend/
