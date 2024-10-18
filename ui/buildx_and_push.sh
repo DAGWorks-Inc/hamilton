28799a98b20da1e66bb4ff923af2e3ee5826bc2b
sikehish <hisham0502@gmail.com>
2024-10-18 23:52:18 +0530

    buildx_and_push.sh: Added functionality to fecth the latest version from PyPi

--- ui/buildx_and_push.sh
+++ ui/buildx_and_push.sh
@@ -2,13 +2,22 @@
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
-# Check if Buildx is already enabled; create a builder instance if not
+#fetches the latest version of sf-hamilton-ui from PyPI
+get_latest_version() {
+    curl -s https://pypi.org/pypi/sf-hamilton-ui/json | \
+    jq -r '.info.version'
+}
+
+VERSION=$(get_latest_version)
+
+echo "Using sf-hamilton-ui version: $VERSION"
+
+# check if Buildx is already enabled; create a builder instance if not
 docker buildx inspect hamilton-builder > /dev/null 2>&1 || \
     docker buildx create --use --name hamilton-builder
 
 FRONTEND_IMAGE="dagworks/ui-frontend"
 BACKEND_IMAGE="dagworks/ui-backend"
-VERSION="latest" # can be modified to a desired version
 
 docker buildx build --platform linux/amd64,linux/arm64 \
     -t $BACKEND_IMAGE:$VERSION -t $BACKEND_IMAGE:latest \
