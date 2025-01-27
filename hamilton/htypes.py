225e91b0bb1241f371c68150333b4f032fe03b0a
Elton Cardoso do Nascimento <43186596+EltonCN@users.noreply.github.com>
2025-01-27 13:19:21 -0300

    Conflict from commit revert fix

--- hamilton/htypes.py
+++ hamilton/htypes.py
@@ -300,19 +300,6 @@ class Parallelizable(Iterable[ParallelizableElement], Protocol[ParallelizableEle
     pass
 
 
-class ParallelizableList(
-    List[ParallelizableElement], Parallelizable, Generic[ParallelizableElement]
-):
-    """
-    Marks the output of a function node as parallelizable and also as a list.
-
-    It has the same usage as "Parallelizable", but for returns that are specifically
-    lists, for correct functioning of linters and other tools.
-    """
-
-    pass
-
-
 def is_parallelizable(type: Type) -> bool:
     """
     Checks if a type is parallelizable.
