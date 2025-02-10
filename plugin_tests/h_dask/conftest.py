4b9e2cdc97022e732d3d89e91413db1af4ccef4b
Stefan Krawczyk <stefan@dagworks.io>
2025-02-10 10:31:39 -0800

    Remove now incorrect config from dask test

--- plugin_tests/h_dask/conftest.py
+++ plugin_tests/h_dask/conftest.py
@@ -1,9 +1,4 @@
-import dask
-
 from hamilton import telemetry
 
 # disable telemetry for all tests!
 telemetry.disable_telemetry()
-
-# required until we fix the DataFrameResultBuilder to work with dask-expr
-dask.config.set({"dataframe.query-planning": False})
