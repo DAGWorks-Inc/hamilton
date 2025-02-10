970f3626f79511acba8972619495e2fb92b9a634
Stefan Krawczyk <stefan@dagworks.io>
2025-02-10 11:34:44 -0800

    Add case for dask + py3.9 test
    dask expr was rolled into dask, but not for 3.9.

--- plugin_tests/h_dask/conftest.py
+++ plugin_tests/h_dask/conftest.py
@@ -2,3 +2,11 @@ from hamilton import telemetry
 
 # disable telemetry for all tests!
 telemetry.disable_telemetry()
+
+# dask_expr got made default, except for python 3.9 and below
+import sys
+
+if sys.version_info < (3, 10):
+    import dask
+
+    dask.config.set({"dataframe.query-planning": False})
