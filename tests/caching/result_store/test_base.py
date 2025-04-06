57d5383d6def90146e56532929197bd4bd970f6a
Charles Swartz <charles.w.swartz.vi@gmail.com>
2025-04-05 21:55:38 -0400

    Separate results and cache store locations
    Note: Colocation of the results and cache store w was causing issues with Window's file share locking.

--- tests/caching/result_store/test_base.py
+++ tests/caching/result_store/test_base.py
@@ -16,7 +16,8 @@ def _instantiate_result_store(result_store_cls, tmp_path):
 
 
 @pytest.fixture
-def result_store(request, tmp_path):
+def result_store(request, tmp_path_factory):
+    tmp_path = tmp_path_factory.mktemp("result_store")
     result_store_cls = request.param
     result_store = _instantiate_result_store(result_store_cls, tmp_path)
 
